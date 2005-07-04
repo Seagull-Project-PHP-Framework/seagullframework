859f97d2c9b7f81b6607d062bb900468a2fea6c3
Demian Turner <demian@seagullsystems.com>
2005-07-04 00:11:19 +0000

    moved block_assignment fk check to 'nav' module - reverted

--- modules/navigation/data/constraints.my.sql
+++ modules/navigation/data/constraints.my.sql
@@ -1,5 +1,2 @@
 alter table category add constraint FK_parent foreign key (parent_id)
       references category (category_id) on delete restrict on update restrict;
-      
-alter table block_assignment add constraint FK_block_assignment foreign key (section_id)
-      references section (section_id) on delete restrict on update restrict;
