101652786d99b6d1593a82ac7c2239813237946f
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-08-01 10:04:52 +0000

    creating translation2 branch

---
+++ branches/translation2/modules/rate/lang/romanian-iso-8859-1.php
@@ -0,0 +1,11 @@
+<?php
+$words['Exchange :: Edit'] = 'Curs valutar :: Edit';
+$words['Data saved successfully'] = 'Date salvate cu succes';
+$words['Update exchange'] = 'Actualizare curs';
+$words['Invalid data'] = 'Date ivalide';
+$words['Value'] = 'Valoare';
+$words['EUR'] = 'Euro';
+$words['USD'] = 'USD';
+$words['ROL'] = 'Lei';
+$words['RON'] = 'Lei noi';
+?>
