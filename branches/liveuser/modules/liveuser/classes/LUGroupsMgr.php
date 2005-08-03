3b293a3a0bb2fbbe57db7b226908dfbf07648c6f
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-08-03 11:35:44 +0000

    small cleanup - 4

--- branches/liveuser/modules/liveuser/classes/LUGroupsMgr.php
+++ branches/liveuser/modules/liveuser/classes/LUGroupsMgr.php
@@ -232,7 +232,7 @@ class LUGroupsMgr extends SGL_Manager
         $output->pageTitle = $this->pageTitle . ' :: Rights';
         
         $group = $this->getGroup($input->group_id);
-        if($group === false) {
+        if ($group === false) {
             LUAdmin::noRecordRedirect();
         }
         $output->group = &$group;
