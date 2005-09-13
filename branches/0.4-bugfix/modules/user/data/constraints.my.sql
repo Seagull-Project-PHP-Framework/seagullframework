c3b60ebfaae32981b0762e4348419cbf68795b4b
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-09-13 10:06:31 +0000

    trial removing user_pref constraints -1

--- branches/0.4-bugfix/modules/user/data/constraints.my.sql
+++ branches/0.4-bugfix/modules/user/data/constraints.my.sql
@@ -1,14 +1,14 @@
-ALTER TABLE `role_permission` ADD FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE CASCADE;
-ALTER TABLE `role_permission` ADD FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`) ON DELETE CASCADE;
+-- ALTER TABLE `role_permission` ADD FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE CASCADE;
+-- ALTER TABLE `role_permission` ADD FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`) ON DELETE CASCADE;
 
-ALTER TABLE `user_permission` ADD FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`) ON DELETE CASCADE;
-ALTER TABLE `user_permission` ADD FOREIGN KEY (`usr_id`) REFERENCES `usr` (`usr_id`) ON DELETE CASCADE;
+-- ALTER TABLE `user_permission` ADD FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`) ON DELETE CASCADE;
+-- ALTER TABLE `user_permission` ADD FOREIGN KEY (`usr_id`) REFERENCES `usr` (`usr_id`) ON DELETE CASCADE;
 
-ALTER TABLE `user_preference` ADD FOREIGN KEY (`usr_id`) REFERENCES `usr` (`usr_id`) ON DELETE CASCADE;
-ALTER TABLE `user_preference` ADD FOREIGN KEY (`preference_id`) REFERENCES `preference` (`preference_id`) ON DELETE CASCADE;
+-- ALTER TABLE `user_preference` ADD FOREIGN KEY (`usr_id`) REFERENCES `usr` (`usr_id`) ON DELETE CASCADE;
+-- ALTER TABLE `user_preference` ADD FOREIGN KEY (`preference_id`) REFERENCES `preference` (`preference_id`) ON DELETE CASCADE;
 
-ALTER TABLE `org_preference` ADD FOREIGN KEY (`organisation_id`) REFERENCES `organisation` (`organisation_id`) ON DELETE CASCADE;
-ALTER TABLE `org_preference` ADD FOREIGN KEY (`preference_id`) REFERENCES `preference` (`preference_id`) ON DELETE CASCADE;
+-- ALTER TABLE `org_preference` ADD FOREIGN KEY (`organisation_id`) REFERENCES `organisation` (`organisation_id`) ON DELETE CASCADE;
+-- ALTER TABLE `org_preference` ADD FOREIGN KEY (`preference_id`) REFERENCES `preference` (`preference_id`) ON DELETE CASCADE;
 
 -- alter table login add constraint FK_usr_login foreign key (usr_id)
 --       references usr (usr_id) on delete restrict on update restrict;
@@ -18,3 +18,39 @@ ALTER TABLE `org_preference` ADD FOREIGN KEY (`preference_id`) REFERENCES `prefe
 -- 
 -- alter table user_preference add constraint FK_usr_user_preferences foreign key (usr_id)
 --       references usr (usr_id) on delete restrict on update restrict;
+
+
+ALTER TABLE role_permission ADD constraint FK_role_role_id
+    FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE cascade;
+
+ALTER TABLE role_permission ADD constraint FK_permission_permission_id
+    FOREIGN KEY (permission_id) REFERENCES permission (permission_id) ON DELETE cascade;
+
+ 
+
+ALTER TABLE user_permission ADD constraint FK_usr_usr_id
+    FOREIGN KEY (usr_id) REFERENCES usr (usr_id) ON DELETE cascade;
+
+ALTER TABLE user_permission ADD constraint FK_permission_permission_id
+    FOREIGN KEY (permission_id) REFERENCES permission (permission_id) ON DELETE cascade;
+
+ 
+
+ALTER TABLE user_preference ADD constraint FK_preference_preference_id
+    FOREIGN KEY (preference_id) REFERENCES preference (preference_id) ON DELETE cascade;
+
+ALTER TABLE user_preference ADD constraint FK_usr_usr_id
+    FOREIGN KEY (usr_id) REFERENCES usr (usr_id) ON DELETE cascade;
+
+ 
+
+ALTER TABLE org_preference ADD constraint FK_organisation_organisation_id     
+    FOREIGN KEY (organisation_id) REFERENCES organisation (organisation_id) ON DELETE cascade;
+
+ALTER TABLE org_preference ADD constraint FK_preference_preference_id
+    FOREIGN KEY (preference_id) REFERENCES preference (preference_id) ON DELETE cascade;
+
+ 
+
+ALTER TABLE login ADD constraint FK_usr_usr_id
+    FOREIGN KEY (usr_id) REFERENCES usr (usr_id) ON DELETE restrict ON UPDATE restrict;
