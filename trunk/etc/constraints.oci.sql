85bede4cf3d3ffd2b122e0f6aaa19ff22918dfb1
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-09-19 07:58:53 +0000

    oci schema/data fixes

--- trunk/etc/constraints.oci.sql
+++ trunk/etc/constraints.oci.sql
@@ -1,6 +1,3 @@
--- ==============================================================
---  create foreign keys
--- ==============================================================
 alter table item add constraint FK_item_item_type foreign key (item_type_id) references item_type (item_type_id);
 alter table item_addition add constraint FK_item_item_addition foreign key (item_id) references item (item_id) on delete cascade;
 alter table item_addition add constraint FK_item_type_mapping_item_add foreign key (item_type_mapping_id) references item_type_mapping (item_type_mapping_id);
