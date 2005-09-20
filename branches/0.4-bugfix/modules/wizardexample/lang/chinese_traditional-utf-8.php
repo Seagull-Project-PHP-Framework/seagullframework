ec5ecc19023d8c1bf944c95e3c1539f37ec233b8
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-09-20 13:55:00 +0000

    Chinese language update

---
+++ branches/0.4-bugfix/modules/wizardexample/lang/chinese_traditional-utf-8.php
@@ -0,0 +1,26 @@
+<?php
+$words = array(
+    'Comment' => 'Comment',
+    'Contact Enquiry from' => 'Contact Enquiry from',
+    'Contact Us' => 'Contact Us',
+    'Email' => '電子郵件',
+    'First Name' => '名',
+    'Last Name' => '姓',
+    'Page1Wiz' => 'Page 1',
+    'Page2Wiz' => 'Page 2',
+    'Type of enquiry' => 'Type of enquiry',
+    'You must enter your email' => '您必須輸入電子郵件地址',
+    'You must enter your first name' => '必須填寫名字',
+    'You must enter your last name' => '必須填寫姓氏',
+    'You must fill in your comment' => 'You must fill in your comment',
+    'Your email is not correctly formatted' => '您輸入的電子郵件地址格式有問題',
+    'aContactType' => array(
+        'General enquiry' => 'General enquiry',
+        'Get a quote' => 'Get a quote',
+        'Hosting info' => 'Hosting info',
+        'Site feedback' => 'Site feedback',
+    ),
+    'contact intro' => 'If you would like to get in touch please fill out the following form and submit your details.',
+    'wizardexample' => 'Wizard Example',
+);
+?>
