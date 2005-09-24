72026a705833b677caf190a23271462baa0b58f8
Demian Turner <demian@seagullsystems.com>
2005-09-24 08:58:58 +0000

    fixed copyright

--- lib/SGL/Locale.php
+++ lib/SGL/Locale.php
@@ -1,7 +1,7 @@
 <?php
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Copyright (c) 2005, Jacob Singh                                        |
+// | Copyright (c) 2005, Demian Turner                                         |
 // | All rights reserved.                                                      |
 // |                                                                           |
 // | Redistribution and use in source and binary forms, with or without        |
