4657632591c6aed1f5f98829731be616605a963a
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-10-10 14:12:29 +0000

    added missing files

---
+++ trunk/www/themes/default/css/vars.php
@@ -0,0 +1,54 @@
+<?
+    $fontFamily             = '"Bitstream Vera Sans", Trebuchet MS, Verdana, Arial, Helvetica, sans-serif';
+    $fontSize               = 'small';
+
+    $primary                = '#99cc00'; // lime green
+    $primaryLight           = '#bbe713'; // light green
+    $primaryText            = '#e6ffa2'; // pale white for text on lime green
+    $primaryTextLight       = '#ffffff'; // white
+    $secondaryLight         = '#e5f1ff'; // baby blue
+    $secondary              = '#9dcdfe'; // blue
+    $secondaryMedium        = '#3399ff'; // medium blue
+    $secondaryDark          = '#184a84'; // dark blue
+    $tertiary               = '#d9d9d9'; // normal gray
+    $tertiaryLight          = '#efefef'; // light gray
+    $tertiaryMedium         = '#bcbcbc'; // medium gray
+    $tertiaryDark           = '#999999'; // dark gray
+    $tertiaryDarker         = '#666666'; // darker gray
+
+    $blocksMarginTop        = '97px';
+    $blocksWidthLeft        = '180px';
+    $blocksWidthRight       = '180px';
+    $blocksBorderBody       = $tertiaryMedium;
+    $blocksBorderTitle      = $tertiaryMedium;
+    $blocksBackgroundBody   = $tertiaryLight;
+    $blocksBackgroundTitle  = $primary;
+    $blocksColorBody        = $secondaryDark;
+    $blocksColorTitle       = $primaryText;
+
+    $tableRowLight          = $tertiaryLight;
+    $tableRowDark           = $tertiary;
+
+    /* Publisher */
+    $sectionHeaderBackground = $tertiary;
+    $sectionHeaderColor     = $secondaryDark;
+    $colHeaderBackground    = $tertiaryLight;
+    $colHeaderColor         = $secondaryDark;
+    $navigatorBackground    = $tertiaryDarker;
+    $navigatorColor         = $tertiaryMedium;
+    
+    $forApproval            = '#ff0000';
+    $approved               = '#ff9933';
+    $published              = '#00cc00';
+    $archived               = '#909090';
+    
+    $error                  = '#ffcc00';
+    $errorLight             = '#ffff99';
+    $errorDark              = '#ff9600';
+    $errorText              = $secondaryDark;
+    $errorTextLight         = '#ffffcc';
+    $errorTextMedium        = '#ff0000';
+
+    /* Button like border colors */
+    $buttonBorderColors     = '#ffffff #333333 #333333 #ffffff';
+?>
