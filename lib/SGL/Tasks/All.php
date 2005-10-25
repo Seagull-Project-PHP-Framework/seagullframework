060f533e5863352f4048cd8a1c7879e90412edde
Demian Turner <demian@seagullsystems.com>
2005-10-25 11:54:02 +0000

    system detection enforcing minimum requirements complete

--- lib/SGL/Tasks/All.php
+++ lib/SGL/Tasks/All.php
@@ -1,9 +1,21 @@
 <?php
+define('EOL', "\n");
+
+//  dependency types
+define('SGL_NEUTRAL', 0);
+define('SGL_RECOMMENDED', 1);
+define('SGL_REQUIRED', 2);
+
 require_once dirname(__FILE__) . '/../Task.php';
 
 function bool2words($key)
 {
     return ($key === true || $key === 1) ? 'Yes' : 'No';        
+}
+
+function bool2int($key)
+{
+    return ($key === true || $key === 1) ? 1 : 0;
 }
 
 function ini_get2($key)
@@ -14,32 +26,79 @@ function ini_get2($key)
 class SGL_EnvSummaryTask extends SGL_Task 
 {
     var $aData = array();
+    var $aRequirements = array();
     var $title = '';
     var $mandatory = false;
     
     function render()
     {
-        $html = '<table width="70%" border=1>';
-        $html .= '<th colspan="3">'.$this->title.'</th>';
+        $html = '<table width="70%" border=1>'.EOL;
+        $html .= '<th colspan="3">'.$this->title.'</th>'.EOL;
         if (!$this->mandatory) {
-            $html .= '<tr><td>&nbsp;</td><td>Recommended</td><td>Actual</td></tr>';        
+            $html .= '<tr><td>&nbsp;</td><td><em>Recommended</em></td><td><em>Actual</em></td></tr>'.EOL;
         }
         foreach ($this->aData as $k => $v) {
-               //   3 cols: element name, recommended, actual
-            $html .= '<tr>';
+            $discoveredValue = (is_int($v)) ? bool2words($v) : $v;
+            $html .= '<tr>'.EOL;
             $html .= '<td><strong>'.$k.'</strong></td>';               
             if (is_array($v)) {
                 $html .= '<td colspan="2">'.$this->createComboBox($v).'</td>';
             } elseif ($this->mandatory) {
-                $html .= '<td colspan="2">'.$v.'</td>';
+                $html .= '<td colspan="2">'.$this->processDependency($this->aRequirements[$k], $v).$discoveredValue.'</span></td>';
             } else {
-                $html .= '<td>'.$v.'</td>';
-                $html .= '<td>'.$v.'</td>';
+                $html .= '<td>'.$this->processRecommended($this->aRequirements[$k]).'</td>';
+                $html .= '<td>'.$this->processDependency($this->aRequirements[$k], $v).$discoveredValue.'</span></td>';
             }
             $html .= '</tr>';
         }
-        $html .= '</table>';
+        $html .= '</table>'.EOL;
         return $html;
+    }
+    
+    function processDependency($aRequirement, $actual)
+    {
+        $depType = key($aRequirement);
+        $depValue = $aRequirement[$depType];// what value the dep requires
+        
+        if ($depType == SGL_REQUIRED) {
+            
+            //  small exception for php version check
+            if (preg_match("/>.*/", $depValue)) {
+                $comparator = $depValue{0};
+                $value = substr($depValue, 1);
+                if (version_compare($actual, $value, 'ge')) {
+                    $status = 'green';                
+                } else {
+                    $status = 'red';
+                }
+                
+            //  else evaluate conventional values
+            } else {
+                if ($actual == $depValue) {
+                    $status = 'green';
+                } else {
+                    $status = 'red';
+                }
+            }
+        } elseif ($depType == SGL_RECOMMENDED) {
+            if ($actual == $depValue) {
+                $status = 'green';
+            } else {
+                $status = 'orange';
+            }
+        } else {
+            //  neutral, no colour tag
+            return '';
+        }
+        $html = "<span style=\"color:$status\">";
+        return $html;
+    }
+    
+    function processRecommended($aRequirement)
+    {
+        $depType = key($aRequirement);
+        $depValue = $aRequirement[$depType];        
+        return is_int($depValue) ? bool2words($depValue) : $depValue;
     }
     
     function createComboBox($aData)
@@ -56,13 +115,28 @@ class SGL_EnvSummaryTask extends SGL_Task
 class SGL_Task_GetLoadedModules extends SGL_EnvSummaryTask
 {
     var $title = 'Available Modules';
-    
+    var $aRequirements = array(
+        'zlib' => array(SGL_RECOMMENDED => 1),
+        'iconv' => array(SGL_RECOMMENDED => 1),
+        'session' => array(SGL_REQUIRED => 1),
+        'gd' => array(SGL_RECOMMENDED => 1),
+        'mysql' => array(SGL_NEUTRAL => 0),
+        'mysqli' => array(SGL_NEUTRAL => 0),
+        'pgsql' => array(SGL_NEUTRAL => 0),
+        'oci8' => array(SGL_NEUTRAL => 0),
+        'curl' => array(SGL_RECOMMENDED => 1),
+        'open_ssl' => array(SGL_RECOMMENDED => 1),
+        'pcre' => array(SGL_REQUIRED => 1),
+        'posix' => array(SGL_RECOMMENDED => 1),
+        'domxml' => array(SGL_RECOMMENDED => 1),
+        'tidy' => array(SGL_RECOMMENDED => 1),
+        'apc' => array(SGL_RECOMMENDED => 1),
+        );
+
     function run()
     {
-    	foreach (array(
-	           'zlib', 'iconv', 'session', 'gd', 'mysql', 'mysqli', 'pgsql', 'oci8',
-	           'curl', 'open_ssl', 'pcre', 'tokenizer', 'posix', 'domxml', 'tidy', 'apc') as $m) {
-    		$this->aData[$m] = bool2words(extension_loaded($m));
+    	foreach ($this->aRequirements as $m => $dep) {
+    		$this->aData[$m] = bool2int(extension_loaded($m));
     	}
     	return $this->render($this->aData);
     }   
@@ -72,6 +146,11 @@ class SGL_Task_GetPhpEnv extends SGL_EnvSummaryTask
 {
     var $title = 'PHP Environment';
     var $mandatory = true;
+    var $aRequirements = array(
+        'phpVersion' => array(SGL_REQUIRED => '>4.3.0'),
+        'operatingSystem' => array(SGL_NEUTRAL => 0),
+        'webserverSapi' => array(SGL_NEUTRAL => 1),
+    );
     
     function run()
     {
@@ -85,6 +164,17 @@ class SGL_Task_GetPhpEnv extends SGL_EnvSummaryTask
 class SGL_Task_GetPhpIniValues extends SGL_EnvSummaryTask
 {
     var $title = 'php.ini Settings';
+    var $aRequirements = array(
+        'safe_mode' => array(SGL_RECOMMENDED => 0),
+        'register_globals' => array(SGL_RECOMMENDED => 0),
+        'magic_quotes_gpc' => array(SGL_RECOMMENDED => 0),
+        'magic_quotes_runtime' => array(SGL_RECOMMENDED => 0),
+        'session.use_trans_sid' => array(SGL_RECOMMENDED => 0),
+        'allow_url_fopen' => array(SGL_RECOMMENDED => 0),
+        'file_uploads' => array(SGL_RECOMMENDED => 1),
+        'post_max_size' => array(SGL_RECOMMENDED => '10MB'),
+        'upload_max_filesize' => array(SGL_RECOMMENDED => '10MB'),
+        );    
     
     function run()
     {
@@ -106,12 +196,18 @@ class SGL_Task_GetFilesystemInfo extends SGL_EnvSummaryTask
     var $title = 'Filesystem info';
     var $mandatory = true;
     
+    var $aRequirements = array(
+        'installRoot' => array(SGL_NEUTRAL => 0),
+        'varDirExists' => array(SGL_REQUIRED => 1),
+        'varDirIsWritable' => array(SGL_REQUIRED => 1),
+    );
+    
     function run()
     {
         $installRoot = dirname(dirname(dirname(dirname(__FILE__))));
         $this->aData['installRoot'] = $installRoot;
-        $this->aData['varDirExists'] = bool2words(file_exists($installRoot . '/var'));
-        $this->aData['varDirIsWritable'] = bool2words(is_writable($installRoot . '/var'));
+        $this->aData['varDirExists'] = bool2int(file_exists($installRoot . '/var'));
+        $this->aData['varDirIsWritable'] = bool2int(is_writable($installRoot . '/var'));
     	return $this->render($this->aData);
     }   
 }
@@ -121,19 +217,30 @@ class SGL_Task_GetPearInfo extends SGL_EnvSummaryTask
     var $title = 'PEAR Environment';
     var $mandatory = true;
     
+    var $aRequirements = array(
+        'pearFolderExists' => array(SGL_REQUIRED => 1),
+        'pearLibIsLoadable' => array(SGL_REQUIRED => 1),
+        'pearPath' => array(SGL_NEUTRAL => 0),
+        'pearSystemLibIsLoadable' => array(SGL_REQUIRED => 1),
+        'pearRegistryLibIsLoadable' => array(SGL_REQUIRED => 1),
+        'pearRegistryIsObject' => array(SGL_REQUIRED => 1),
+        'pearBundledPackages' => array(SGL_NEUTRAL => 0),
+    );
+    
     function run()
     {
         $installRoot = dirname(dirname(dirname(dirname(__FILE__))));
-        $this->aData['pearFolderExists'] = bool2words(file_exists($installRoot . '/lib/pear'));
-        $this->aData['pearLibIsLoadable'] = bool2words(include_once $installRoot . '/lib/pear/PEAR.php');
+        $this->aData['pearFolderExists'] = bool2int(file_exists($installRoot . '/lib/pear'));
+        $this->aData['pearLibIsLoadable'] = bool2int(include_once $installRoot . '/lib/pear/PEAR.php');
         
         $includeSeparator = (substr(PHP_OS, 0, 3) == 'WIN') ? ';' : ':';
-        $this->aData['pearPathSetup'] = @ini_set('include_path',      '.' . $includeSeparator . $installRoot . '/lib/pear');
-        $this->aData['pearSystemLibIsLoadable'] = bool2words(require_once 'System.php');
-        $this->aData['pearRegistryLibIsLoadable'] = bool2words(require_once 'PEAR/Registry.php');
+        $ok = @ini_set('include_path',      '.' . $includeSeparator . $installRoot . '/lib/pear');
+        $this->aData['pearPath'] = @ini_get('include_path');
+        $this->aData['pearSystemLibIsLoadable'] = bool2int(require_once 'System.php');
+        $this->aData['pearRegistryLibIsLoadable'] = bool2int(require_once 'PEAR/Registry.php');
         $registry = new PEAR_Registry($installRoot . '/lib/pear');
-        $this->aData['pearRegistryIsObject'] = bool2words(is_object($registry));
-        $this->aData['pearPackages'] = $registry->_listPackages();
+        $this->aData['pearRegistryIsObject'] = bool2int(is_object($registry));
+        $this->aData['pearBundledPackages'] = $registry->_listPackages();
     	return $this->render($this->aData);
     }   
 }
--- www/setup.php
+++ www/setup.php
@@ -277,7 +277,7 @@ function sgl_header()
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-    <title>Seagull Framework :: FAQ</title>        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
+    <title>Seagull Framework :: Setup</title>        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
     <meta http-equiv="Content-Language" content="en" />
     <meta name="ROBOTS" content="ALL" />
     <meta name="Copyright" content="Copyright (c) 2005 Seagull Framework, Demian Turner, and the respective authors" />
