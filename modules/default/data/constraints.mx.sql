9953f2547e301b9fb63d5bcf440f26c156bb8cbf
Demian Turner <demian@seagullsystems.com>
2005-11-02 14:07:43 +0000

    schema + data + constraints now working correctly

--- modules/default/data/constraints.mx.sql
+++ modules/default/data/constraints.mx.sql
@@ -1,17 +1,17 @@
--- ==========================================================================
--- Seagull PHP Framework: Default Data for PostgreSQL
--- ==========================================================================
+-- ==============================================================
+--  DBMS name:      MaxDB 7.3
+--  Created on:     2004-04-13 23:45:57
+-- ==============================================================
 
--- Schema for /modules/default
+-- Constraints for /etc
 
 
--- Begin a transaction
--- This is not really necessary, but is very useful in developing phase. ;-)
+-- Begin a transaction-- This is not really necessary, but is very useful in developing phase. ;-)
 --
 
--- ==============================================================
---  Table: module
--- ==============================================================
-
+ALTER TABLE item ADD FOREIGN KEY (item_type_id) REFERENCES item_type;
+ALTER TABLE item_addition ADD FOREIGN KEY (item_id) REFERENCES item;
+ALTER TABLE item_addition ADD FOREIGN KEY (item_type_mapping_id) REFERENCES item_type_mapping;
+ALTER TABLE item_type_mapping ADD FOREIGN KEY (item_type_id) REFERENCES item_type;
 
 COMMIT;
