a9383651abdaecc844a441b585315628f6146c31
Demian Turner <demian@seagullsystems.com>
2005-11-11 13:16:52 +0000

    hard-coded path in SGL_Install::printHeader() fixed

--- lib/SGL/Install.php
+++ lib/SGL/Install.php
@@ -103,6 +103,7 @@ EOF;
 
     function printHeader($title = '')
     {
+        $baseUrl = SGL_BASE_URL;
         $html = <<<HTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
@@ -117,7 +118,7 @@ EOF;
     <link rel="help" href="http://seagull.phpkitchen.com/docs/" title="Seagull Documentation." />
 
     <style type="text/css" media="screen">
-        @import url("http://localhost/seagull/trunk/www/themes/default/css/style.php?navStylesheet=SglDefault_TwoLevel&moduleName=default");
+        @import url("$baseUrl/themes/default/css/style.php?navStylesheet=SglDefault_TwoLevel&moduleName=default");
     </style>
     </head>
 <body>
