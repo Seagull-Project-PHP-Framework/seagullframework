cb29c63715aae4853051a08a218c98c7a60f98fe
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-11-15 13:07:25 +0000

    oci updates applied

--- trunk/modules/publisher/data/data.default.oci.sql
+++ trunk/modules/publisher/data/data.default.oci.sql
@@ -1,4 +1,78 @@
 --
+-- Dumping data for table module
+--
+
+INSERT INTO module VALUES ({SGL_NEXT_ID}, 1, 'publisher', 'Publisher', 'The ''Publisher'' module allows you to create content and publish it to your site.  Currently you can create various types of articles and upload and categorise any filetype, matching the two together in a browsable archive format.', 'publisher/article', 'publisher.png');
+
+--
+-- Dumping data for table permission
+--
+
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr_add', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr_insert', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr_edit', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr_update', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr_delete', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'contenttypemgr_list', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'wikiscrapemgr', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'wikiscrapemgr_list', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articleviewmgr', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'filemgr', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_add', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_insert', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_edit', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_update', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_setDownload', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_view', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_delete', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'documentmgr_list', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'filemgr_download', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'filemgr_downloadZipped', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'filemgr_view', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articleviewmgr_view', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articleviewmgr_summary', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_add', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_insert', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_edit', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_update', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_changeStatus', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_delete', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_view', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'articlemgr_list', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr_insert', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr_update', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr_list', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr_delete', '', (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr_reorder', NULL, (SELECT MAX(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'categorymgr_reorderUpdate', NULL, (SELECT MAX(module_id) FROM module));
+
+-- guest role perms
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 0, (SELECT permission_id FROM permission WHERE name = 'articleviewmgr_summary'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 0, (SELECT permission_id FROM permission WHERE name = 'articleviewmgr_view'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 0, (SELECT permission_id FROM permission WHERE name = 'filemgr_download'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 0, (SELECT permission_id FROM permission WHERE name = 'filemgr_downloadZipped'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 0, (SELECT permission_id FROM permission WHERE name = 'filemgr_view'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 0, (SELECT permission_id FROM permission WHERE name = 'wikiscrapemgr_list'));
+
+-- member role perms
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articlemgr_add'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articlemgr_edit'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articlemgr_insert'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articlemgr_list'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articlemgr_update'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articlemgr_view'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articleviewmgr_summary'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'articleviewmgr_view'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'filemgr_download'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'filemgr_downloadZipped'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'filemgr_view'));
+INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, (SELECT permission_id FROM permission WHERE name = 'wikiscrapemgr_list'));
+
+--
 -- Dumping data for table document_type
 --
 
@@ -9,7 +83,6 @@ INSERT INTO document_type VALUES (4, 'URL');
 INSERT INTO document_type VALUES (5, 'Image');
 INSERT INTO document_type VALUES (6, 'PDF');
 INSERT INTO document_type VALUES (7, 'unknown');
-
 
 --
 -- Dumping data for table item
@@ -45,6 +118,23 @@ INSERT INTO item_type_mapping VALUES (7, 5, 'title', 0);
 INSERT INTO item_type_mapping VALUES (8, 5, 'bodyHtml', 2);
 
 --
+-- Dumping data for table category
+--
+
+INSERT INTO category VALUES (0,'HiddenRoot','',0,0,0,0,0,0);
+INSERT INTO category VALUES (1,'PublisherRoot',NULL,0,1,1,4,1,1);
+INSERT INTO category VALUES (2,'example',NULL,1,1,2,3,1,2);
+INSERT INTO category VALUES (3,'OtherRoot',NULL,0,3,1,2,1,1);
+INSERT INTO category VALUES (4,'Shop',NULL,0,4,1,16,2,1);
+INSERT INTO category VALUES (6,'Printers',NULL,4,4,8,9,2,2);
+INSERT INTO category VALUES (5,'Monitors',NULL,4,4,2,7,1,2);
+INSERT INTO category VALUES (13,'CRT',NULL,5,4,3,4,1,3);
+INSERT INTO category VALUES (7,'Laptop Computers',NULL,4,4,10,15,3,2);
+INSERT INTO category VALUES (9,'Notebook',NULL,7,4,11,12,1,3);
+INSERT INTO category VALUES (11,'Tablet PC',NULL,7,4,13,14,2,3);
+INSERT INTO category VALUES (15,'LCD',NULL,5,4,5,6,2,3);
+
+--
 -- Creating sequences
 -- sequence must start on the first free record id
 --
@@ -54,21 +144,4 @@ CREATE SEQUENCE item_seq START WITH 2;
 CREATE SEQUENCE item_addition_seq START WITH 3;
 CREATE SEQUENCE item_type_seq START WITH 6;
 CREATE SEQUENCE item_type_mapping_seq START WITH 9;
-CREATE SEQUENCE category_seq START WITH 4;
-
---
--- Dumping data for table category
---
-
-INSERT INTO category VALUES (0,'HiddenRoot','',0,0,0,0,0,0);
-INSERT INTO category VALUES (1,'PublisherRoot',NULL,0,1,1,4,1,1);
-INSERT INTO category VALUES (2,'example',NULL,1,1,2,3,1,2);
-INSERT INTO category VALUES (3,'OtherRoot',NULL,0,3,1,2,1,1);
---INSERT INTO category VALUES (4,'Shop',NULL,0,4,1,16,2,1);
---INSERT INTO category VALUES (6,'Printers',NULL,4,4,8,9,2,2);
---INSERT INTO category VALUES (5,'Monitors',NULL,4,4,2,7,1,2);
---INSERT INTO category VALUES (13,'CRT',NULL,5,4,3,4,1,3);
---INSERT INTO category VALUES (7,'Laptop Computers',NULL,4,4,10,15,3,2);
---INSERT INTO category VALUES (9,'Notebook',NULL,7,4,11,12,1,3);
---INSERT INTO category VALUES (11,'Tablet PC',NULL,7,4,13,14,2,3);
---INSERT INTO category VALUES (15,'LCD',NULL,5,4,5,6,2,3);
+CREATE SEQUENCE category_seq START WITH 16;
