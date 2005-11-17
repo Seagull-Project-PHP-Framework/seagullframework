66792eab5267d99181ab29981712d733217cd5a5
Demian Turner <demian@seagullsystems.com>
2005-11-17 15:44:38 +0000

    fixed php tests

--- lib/SGL/tests/ConfigTest.ndb.php
+++ lib/SGL/tests/ConfigTest.ndb.php
@@ -7,19 +7,19 @@ require_once dirname(__FILE__) . '/../Config.php';
  * @package SGL
  * @author  Demian Turner <demian@phpkitchen.net>
  * @version $Id: UrlTest.ndb.php,v 1.1 2005/06/23 14:56:01 demian Exp $
- */  
+ */
 class ConfigTest extends UnitTestCase {
 
     function ConfigTest()
     {
         $this->UnitTestCase('Config Test');
     }
-    
+
     function setup()
     {
-        $this->c = &SGL_Config::singleton();   
+        $this->c = &SGL_Config::singleton();
     }
-    
+
     function testLoadIniFile()
     {
         $file = dirname(__FILE__) . '/test.conf.ini';
@@ -27,7 +27,7 @@ class ConfigTest extends UnitTestCase {
         $this->assertTrue(is_array($ret));
         $this->assertEqual(count($ret), 14);
     }
-    
+
     function testLoadPhpArrayFile()
     {
         $file = dirname(__FILE__) . '/test.conf.php';
@@ -35,39 +35,39 @@ class ConfigTest extends UnitTestCase {
         $this->assertTrue(is_array($ret));
         $this->assertEqual(count($ret), 14);
     }
-    
+
     function testWriteIniFile()
     {
         $file = dirname(__FILE__) . '/test.conf.ini';
         $ret = $this->c->load($file);
         $this->assertTrue(is_array($ret));
         $this->assertEqual(count($ret), 14);
-        
+
         $tmpFileName = tempnam('/tmp', 'test');
         $iniTmpFileName = $tmpFileName . '.ini';
         $ok = $this->c->save($iniTmpFileName);
         $this->assertTrue(file_exists($iniTmpFileName));
         $this->assertTrue(is_array(parse_ini_file($iniTmpFileName)));
     }
-    
-    
+
+
     function testWritePhpArrayFile()
     {
         $file = dirname(__FILE__) . '/test.conf.ini';
         $ret = $this->c->load($file);
         $this->assertTrue(is_array($ret));
         $this->assertEqual(count($ret), 14);
-        
+
         $tmpFileName = tempnam('/tmp', 'test');
         $phpTmpFileName = $tmpFileName . '.php';
         $ok = $this->c->save($phpTmpFileName);
-        $this->assertTrue($ok);        
+        $this->assertTrue($ok);
         $this->assertTrue(file_exists($phpTmpFileName));
         $aConf = $this->c->load($phpTmpFileName);
         $this->assertTrue(is_array($aConf));
-        $this->assertEqual(count($aConf), 19);
+        $this->assertEqual(count($aConf), 20);
     }
-    
+
     function testSetScalarProperty()
     {
         $file = dirname(__FILE__) . '/test.conf.ini';
@@ -75,7 +75,7 @@ class ConfigTest extends UnitTestCase {
         $this->c->set('foo', 'bar');
         $this->assertTrue(array_key_exists('foo', $this->c->getAll()));
     }
-    
+
     function testSetArrayProperty()
     {
         $file = dirname(__FILE__) . '/test.conf.ini';
