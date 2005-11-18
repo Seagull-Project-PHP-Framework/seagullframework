d00f031f174d6f57ac8817d6bc7fb97b7e542c19
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-11-18 18:40:03 +0000

    mysql data updates

--- trunk/modules/messaging/data/data.default.my.sql
+++ trunk/modules/messaging/data/data.default.my.sql
@@ -42,4 +42,4 @@ INSERT INTO role_permission VALUES ({SGL_NEXT_ID}, 2, @permissionId);
 #
 # Dumping data for table `instant_message`
 #
-INSERT INTO instant_message VALUES (1,1,2,'2003-08-14 00:14:41','Welcome to Seagull','This is an example message.  Once you add users to the system, they can contact eachother via this instant message interface.',3,3);
+#INSERT INTO instant_message VALUES (1,1,2,'2003-08-14 00:14:41','Welcome to Seagull','This is an example message.  Once you add users to the system, they can contact eachother via this instant message interface.',3,3);
