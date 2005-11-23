5876c161fc16684c64dd66877c70d897737b8cbb
Demian Turner <demian@seagullsystems.com>
2005-11-23 11:53:38 +0000

    smarty tweaks

--- lib/SGL/HtmlSmartyRendererStrategy.php
+++ lib/SGL/HtmlSmartyRendererStrategy.php
@@ -37,24 +37,37 @@
 // | Author: Malaney J. Hill  <malaney@gmail.com>                              |
 // +---------------------------------------------------------------------------+
 
-define('SGL_SMARTY_DIR', SGL_LIB_DIR .'/smarty/');
-require_once SGL_SMARTY_DIR.'Smarty.class.php';
+define('SGL_SMARTY_DIR', SGL_LIB_DIR .'/other/Smarty');
+require_once SGL_SMARTY_DIR . '/libs/Smarty.class.php';
 
-class SGL_Smarty extends Smarty 
+class SGL_Smarty extends Smarty
 {
     /**
      *  Constructor.
      *  @access public
      *  @return void
      */
-    function SGL_Smarty() 
+    function SGL_Smarty()
     {
         $this->Smarty();
-        $this->debugging = true;
-        $this->template_dir = SGL_WEB_ROOT.'/themes/smarty/';
-        $this->compile_dir = SGL_SMARTY_DIR.'templates_c/';
-        $this->config_dir = SGL_SMARTY_DIR.'configs/';
-        $this->cache_dir = SGL_SMARTY_DIR.'cache/';
+        $this->debugging = false;
+        $this->template_dir = SGL_THEME_DIR . '/smarty';
+        $this->compile_dir = SGL_CACHE_DIR . '/templates_c';
+        $this->config_dir = SGL_SMARTY_DIR . '/unit_test/configs';
+        $this->cache_dir = SGL_CACHE_DIR;
+
+        //  starts session for page execution
+        if (!is_writable($this->compile_dir)) {
+            require_once 'System.php';
+
+            //  pass path as array to avoid widows space parsing prob
+            $success = System::mkDir(array($this->compile_dir));
+            if (!$success) {
+                SGL::raiseError('The tmp directory does not '.
+                'appear to be writable, please give the webserver permissions to write to it',
+                SGL_ERROR_FILEUNWRITABLE, PEAR_ERROR_DIE);
+            }
+        }
     }
     /**
      * Returns a singleton Smarty instance.
@@ -69,7 +82,7 @@ class SGL_Smarty extends Smarty
      * @static
      * @return  mixed reference to SGL_Smarty object
      */
-    function &singleton() 
+    function &singleton()
     {
         static $instance;
         if (!isset($instance)) {
@@ -92,10 +105,10 @@ class SGL_HtmlSmartyRendererStrategy extends SGL_OutputRendererStrategy
     {
         //  invoke html view specific post-process tasks
         $view->postProcess($view);
-        
+
         //  suppress error notices in templates
         SGL::setNoticeBehaviour(SGL_NOTICES_DISABLED);
-        
+
         //  prepare smarty object
         $smarty = &SGL_Smarty::singleton();
 
