f0c4e14b66b8a0e4a1f271894026c2499df547ab
Demian Turner <demian@seagullsystems.com>
2005-11-29 19:41:01 +0000

    first round of work for multi-strategy urls

---
+++ lib/SGL/UrlParserSimpleStrategy.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Strategy for handling URL aliases.
+ *
+ * @package SGL
+ * @author  Demian Turner <demian@phpkitchen.com>
+ * @version $Revision: 1.5 $
+ */
+
+/**
+ * Concrete alias url parser strategy
+ *
+ */
+class SGL_UrlParserSimpleStrategy extends SGL_UrlParserStrategy
+{
+    /**
+     * Analyzes querystring content and parses it into module/manager/action and params.
+     *
+     * @param SGL_Url $url
+     * @return array        An array to be assigned to SGL_Url::aQueryData
+     */
+    function parseQueryString(/*SGL_Url*/$url, $conf)
+    {
+        #$aUriParts = SGL_Url::toPartialArray($url->url, $conf['site']['frontScriptName']);
+        $ret = array();
+        $default = array(
+            'moduleName' => 'default',
+            'manager' => 'default');
+
+        //  catch case for default page, ie, home
+        if (empty($url->url)) {
+            return $default;
+        }
+        $parts = array_filter(explode('/', $url->url), 'strlen');
+        $numElems = count($parts);
+
+        //  we need at least 2 elements
+        if ($numElems < 2) {
+            return $default;
+        }
+        $ret['moduleName'] = $parts[0];
+        $ret['managerName'] = $parts[1];
+        $actionExists = (isset($parts[2]) && $parts[2] == 'action') ? true : false;
+        $ret['action'] = ($actionExists) ? $parts[3] : null;
+
+        //  parse params
+        $idx = ($actionExists) ? 4 : 2;
+
+        //  break out if no params detected
+        if ($numElems <= $idx) {
+            return $ret;
+        }
+        $aTmp = array();
+        for ($x = $idx; $x < $numElems; $x++) {
+            if ($x % 2) { // if index is odd
+                $aTmp['varValue'] = urldecode($parts[$x]);
+            } else {
+                // parsing the parameters
+                $aTmp['varName'] = urldecode($parts[$x]);
+            }
+            //  if a name/value pair exists, add it to request
+            if (count($aTmp) == 2) {
+                $ret['parsed_params'][$aTmp['varName']] = $aTmp['varValue'];
+                $aTmp = array();
+            }
+        }
+        return $ret;
+    }
+}
+
+?>
---
+++ lib/SGL/tests/UrlStrategySimpleTest.ndb.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once dirname(__FILE__) . '/../UrlParserSimpleStrategy.php';
+
+/**
+ * Test suite.
+ *
+ * @package SGL
+ * @author  Demian Turner <demian@phpkitchen.net>
+ * @version $Id: UrlTest.ndb.php,v 1.1 2005/06/23 14:56:01 demian Exp $
+ */
+class UrlStrategySimpleTest extends UnitTestCase
+{
+
+    function UrlStrategySimpleTest()
+    {
+        $this->UnitTestCase('simple strategy test');
+    }
+
+    function testSimpleParser()
+    {
+        $qs = 'user/account';
+        $url = new SGL_Url($qs, true, new SGL_UrlParserSimpleStrategy());
+        $ret = $url->getQueryData();
+
+        //  assert expected keys present
+        $this->assertTrue(array_key_exists('moduleName', $ret));
+        $this->assertTrue(array_key_exists('managerName', $ret));
+
+        //  assert expected values present
+        $this->assertEqual($ret['moduleName'], 'user');
+        $this->assertEqual($ret['managerName'], 'account');
+    }
+}
+?>
