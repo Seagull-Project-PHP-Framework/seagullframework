76e5ff64e4ee48216d8bfeb12dcaa354bd3441e7
Demian Turner <demian@seagullsystems.com>
2005-11-30 02:08:16 +0000

    adding tmp url alias data

---
+++ lib/data/ary.uriAliases.php
@@ -0,0 +1,8 @@
+<?php
+$aUriAliases = array(
+    'seagull-php-framework' => 'default/default',
+	'contact-seagull-php-framework' => 'contactus',
+	'content-reshuffle-alias' => 'publisher/articleview/frmArticleID/1/staticId/6',
+	'static-article' => 'publisher/articleview/frmArticleID/1',
+    );
+?>
