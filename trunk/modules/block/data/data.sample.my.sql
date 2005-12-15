f327722c15c33e70c4145e18a2aea3fbb8c6c7be
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-12-15 12:51:16 +0000

    fixing block data for mysql

--- trunk/modules/block/data/data.sample.my.sql
+++ trunk/modules/block/data/data.sample.my.sql
@@ -8,12 +8,7 @@ INSERT INTO block VALUES (5, 'CategoryNav', 'Categories', '', 'navWidget', 1, 'L
 INSERT INTO block VALUES (10, 'SampleRightBlock1', 'Sample Right Block', '', '', 1, 'Right', 1, NULL);
 INSERT INTO block VALUES (11, 'CalendarBlock', 'Calendar', '', '', 2, 'Right', 0, NULL);
 INSERT INTO block VALUES (12, 'LoginBlock', 'Login', '', '', 3, 'Right', 0, NULL);
-INSERT INTO block VALUES (13, 'ShopNav', 'Products', '', 'sgl-dropdown', 1, 'Left', 0, NULL);
-INSERT INTO block VALUES (14, 'ShoppingCart', 'Cart', '', '', 1, 'Right', 0, NULL);
-INSERT INTO block VALUES (15, 'RndProducts', 'Promotions', '', 'rndProducts', 5, 'Right', 0, NULL);
-INSERT INTO block VALUES (16, 'ShopSearch', 'Search', '', '', 7, 'Left', 0, NULL);
 INSERT INTO block VALUES (17, 'NewsletterBlock', 'Newsletter', '', '', 5, 'Right', 0, NULL);
-INSERT INTO block VALUES (18, 'Exchange', 'Exchange', '', '', 9, 'Left', 0, NULL);
 
 
 #
@@ -23,18 +18,9 @@ INSERT INTO block VALUES (18, 'Exchange', 'Exchange', '', '', 9, 'Left', 0, NULL
 INSERT INTO block_assignment VALUES (1, 0);
 INSERT INTO block_assignment VALUES (3, 0);
 INSERT INTO block_assignment VALUES (4, 0);
-INSERT INTO block_assignment VALUES (5, 2);
-INSERT INTO block_assignment VALUES (5, 17);
-INSERT INTO block_assignment VALUES (5, 18);
-INSERT INTO block_assignment VALUES (5, 19);
 INSERT INTO block_assignment VALUES (10, 0);
-INSERT INTO block_assignment VALUES (11, 0);
-INSERT INTO block_assignment VALUES (12, 0);
-INSERT INTO block_assignment VALUES (13, 22);
-INSERT INTO block_assignment VALUES (13, 24);
-INSERT INTO block_assignment VALUES (16, 0);
+INSERT INTO block_assignment VALUES (11, 0);;
 INSERT INTO block_assignment VALUES (17, 0);
-INSERT INTO block_assignment VALUES (18, 0);
 
 INSERT INTO block_role VALUES (1, -2);
 INSERT INTO block_role VALUES (2, -2);
@@ -44,9 +30,4 @@ INSERT INTO block_role VALUES (5, -2);
 INSERT INTO block_role VALUES (10, -2);
 INSERT INTO block_role VALUES (11, -2);
 INSERT INTO block_role VALUES (12, -2);
-INSERT INTO block_role VALUES (13, -2);
-INSERT INTO block_role VALUES (14, -2);
-INSERT INTO block_role VALUES (15, -2);
-INSERT INTO block_role VALUES (16, -2);
 INSERT INTO block_role VALUES (17, -2);
-INSERT INTO block_role VALUES (18, -2);
