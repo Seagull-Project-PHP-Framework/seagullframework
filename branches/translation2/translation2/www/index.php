63ba5626e5aab462b7c96cefa07603b291c4039a
aj <aj@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-12-16 10:58:12 +0000

    removing everything to replace with rev 474

--- branches/translation2/translation2/www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//    initialise
-    require_once '../init.php';
-    require_once SGL_CORE_DIR . '/Controller.php';
-    $process = & new SGL_Controller();
-    $process->go();
-?>
--- branches/translation2/trunk/modules/documentor/lang/french-utf-8.php
+++
@@ -1,6 +0,0 @@
-<?php
-$defaultWords = array(
-'Manual Generator' => 'Générateur de manuel',
-'no results found' => 'aucun résultat trouvé',
-);
-?>
