8313539933819b099126205ac881e62d2fb00c4e
aj <aj@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-12-19 09:41:13 +0000

    fixed bug in lib/SGL/Install/WizardCreateAdminUser.php that stopped it from properly initializing.

--- branches/translation2/lib/SGL/Install/WizardCreateAdminUser.php
+++ branches/translation2/lib/SGL/Install/WizardCreateAdminUser.php
@@ -53,7 +53,7 @@ class WizardCreateAdminUser extends HTML_QuickForm_Page
 {
     function buildForm()
     {
-        require_once SGL_CORE_DIR .'/Translation.php'+
+        require_once SGL_CORE_DIR .'/Translation.php';
 
         $this->_formBuilt = true;
         $this->addElement('header',     null, 'Create Admin User: page 5 of 5');
