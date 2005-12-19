7c5e350f8a68b1309e6282c78fd454dd54c4f931
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2005-12-19 16:33:59 +0000

    correct  invocation

--- branches/translation2/lib/SGL/Translation.php
+++ branches/translation2/lib/SGL/Translation.php
@@ -39,7 +39,7 @@
 // $Id: Translation.php,v 1.0 2005/05/11 00:00:00 demian Exp $
 
 require_once 'Translation2.php';
-                
+
 /**
  * A wrapper to PEAR Translation2.
  *
@@ -53,24 +53,25 @@ class SGL_Translation
 {
     /**
      * Generate singleton for PEAR::Tranlation2
-     * 
+     *
      * Object types:
      *  o translation (default)
      *  o admin - translation2_admin
      * Storage drivers: (Set in global config under site)
      *  o single - all translations in a single table (translations)
      *  o multiple (default) - all translations in a seperate table (translation_en, translation_pl, translation_de)
-     * 
-     * @access  public 
+     *
+     * @access  public
      * @param	string	$lang			language to return translations
-     * @param	string	$type			type of object: translation or admin 
+     * @param	string	$type			type of object: translation or admin
      * @return	object	$translation	Translation2 object
-     * 
-     * 
+     *
+     *
      */
     function &singleton($type = 'translation')
     {
-        $conf = & $GLOBALS['_SGL']['CONF'];
+        $c = &SGL_Config::singleton();
+        $conf = $c->getAll();
 
         //  set translation table parameters
         $params = array(
@@ -88,64 +89,65 @@ class SGL_Translation
         //  set translation driver
         $driver = 'DB';
 
-        //  retreive DSN  
-        $dsn = SGL_DB::getDsn('SGL_DSN_ARRAY');        
-     
+        //  retreive DSN
+        $dsn = SGL_DB::getDsn('SGL_DSN_ARRAY');
+
         //  create translation storage tables
         if ($conf['table']['translation']) {
-            
+
             //  fetch translation table prefix
             $prefix = $conf['table']['translation'] .'_';
 
             //  fetch languages
             $langs = $GLOBALS['_SGL']['INSTALLED_LANGUAGES'];
-            
+
             //  set params
             foreach ($langs as $aKey => $aValue) {
                 $params['strings_tables'][$aKey] = $prefix . $aKey;
             }
 
         } else {
-            SGL::raiseError('translation table not specified check global config ', 
+            SGL::raiseError('translation table not specified check global config ',
                 SGL_ERROR_INVALIDCONFIG, PEAR_ERROR_DIE);
         }
-                           
-        //  instantiate selected translation2 object
-        switch (strtolower($type)) {           
-            case 'admin':
-                require_once 'Translation2/Admin.php';
-                $oTranslation = &Translation2_Admin::factory($driver, $dsn, $params);
-                break;
-                
-            case 'translation':            
-            default:
-                $oTranslation = &Translation2::factory($driver, $dsn, $params);
-        }        
 
-        return $oTranslation;                
+        //  instantiate selected translation2 object
+        switch (strtolower($type)) {
+
+        case 'admin':
+            require_once 'Translation2/Admin.php';
+            $oTranslation = &Translation2_Admin::factory($driver, $dsn, $params);
+            break;
+
+        case 'translation':
+        default:
+            $oTranslation = &Translation2::factory($driver, $dsn, $params);
+        }
+
+        return $oTranslation;
     }
-    
+
 
     function getGuiTranslationsFromFile($module, $lang)
     {
         //  fetch translations from database and cache
         $cache = &SGL::cacheSingleton();
-        
+
         //  returned cached translations else fetch from db and cache
         if ($serialized = $cache->get($module, 'translation_'. $lang)) {
             $words = unserialize($serialized);
             SGL::logMessage('translations from cache', PEAR_LOG_DEBUG);
             return $words;
-            
+
         } else {
 
             //  fetch available languages
             $aLanguages = $GLOBALS['_SGL']['LANGUAGE'];
-            
+
             //  build global lang file
-            $langID = str_replace('_', '-', $lang);                         
+            $langID = str_replace('_', '-', $lang);
             $language = $aLanguages[$langID][1];
-            $globalLangFile = $language .'.php';     
+            $globalLangFile = $language .'.php';
             $path = SGL_MOD_DIR . '/' . $module . '/lang/';
             if (is_readable($path . $globalLangFile)) {
                 include $path . $globalLangFile;
@@ -154,38 +156,38 @@ class SGL_Translation
                 }
                 $serialized = serialize($words);
                 $cache->save($serialized, $module, 'translation_'. $lang);
-                SGL::logMessage('translations from file', PEAR_LOG_DEBUG);  
+                SGL::logMessage('translations from file', PEAR_LOG_DEBUG);
                 return $words;
-                
+
             } elseif ($module == 'default') {
                 SGL::raiseError('could not locate the global language file', SGL_ERROR_NOFILE);
             }
         }
     }
-    
-    
-    function getTranslations($module, $lang, $fallbackLang = false) 
+
+
+    function getTranslations($module, $lang, $fallbackLang = false)
     {
         if (!empty($module) && !empty($lang)) {
             $conf = &$GLOBALS['_SGL']['CONF'];
-            
+
             //  fallback lang clause
-            $fallbackLang = ($fallbackLang) ? $fallbackLang : $conf['translation']['fallbackLang'];            
+            $fallbackLang = ($fallbackLang) ? $fallbackLang : $conf['translation']['fallbackLang'];
 
             //  if langauge not installed resort to fallback
             if (!array_key_exists($lang, $GLOBALS['_SGL']['INSTALLED_LANGUAGES'])) {
                 $lang = $fallbackLang;
             }
-                            
+
             //  instantiate translation2 object
             $translation = &SGL_Translation::singleton();
-                           
+
             //  set language
             $langInstalled = $translation->setLang($lang);
-                            
+
             //  set translation group
             $translation->setPageID($module);
-                                               
+
             //  create decorator for fallback language
             if ($lang !== $fallbackLang && $fallbackLang) {
                 $translation = & $translation->getDecorator('Lang');
@@ -198,23 +200,23 @@ class SGL_Translation
                 $translation->setOption('cacheDir', SGL_TMP_DIR .'/');
                 $translation->setOption('lifeTime', $conf['cache']['lifetime']);
             }
-            
+
             //  fetch translations
             $words = $translation->getPage();
-            
+
             SGL::logMessage('translations from db for '. $module, PEAR_LOG_DEBUG);
 
             return $words;
-           
+
         } else {
-            SGL::raiseError('Incorrect parameter passed to '.__CLASS__.'::'.__FUNCTION__, SGL_ERROR_INVALIDARGS);    
+            SGL::raiseError('Incorrect parameter passed to '.__CLASS__.'::'.__FUNCTION__, SGL_ERROR_INVALIDARGS);
         }
     }
-    
+
     function getLangID()
     {
         if ($langID = str_replace('-', '_', SGL::getCurrentLang() .'_'. $GLOBALS['_SGL']['CHARSET'])) {
-            return $langID;   
+            return $langID;
         } else {
             return $GLOBALS['_SGL']['CONF']['translation']['fallbackLang'];
         }
@@ -225,13 +227,13 @@ class SGL_Translation
         //  fetch available languages
         require_once SGL_DAT_DIR . '/ary.languages.php';
         $availableLanguages = $GLOBALS['_SGL']['LANGUAGE'];
-    
-        //  sort and return                
+
+        //  sort and return
         uasort($availableLanguages, 'SGL_cmp');
         foreach ($availableLanguages as $id => $tmplang) {
             $langName = ucfirst(substr(strstr($tmplang[0], '|'), 1));
             $aLangOptions[$id] =  $langName .' ('. $id .')';
-        }       
+        }
         return $aLangOptions;
     }
 }
