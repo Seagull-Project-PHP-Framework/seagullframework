0b7092ec08f844127d6768168766d0c41fd223d2
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-02-05 19:19:12 +0000

    implemented bc for array_intersect_key

--- trunk/lib/SGL/Install/WizardCreateAdminUser.php
+++ trunk/lib/SGL/Install/WizardCreateAdminUser.php
@@ -140,4 +140,24 @@ class WizardCreateAdminUser extends HTML_QuickForm_Page
         $this->setDefaultAction('next');
     }
 }
+
+//  it's a php >= 5.1 fn
+if (!function_exists('array_intersect_key')) {
+
+    function array_intersect_key($isec, $arr2)
+    {
+        $argc = func_num_args();
+
+        for ($i = 1; !empty($isec) && $i < $argc; $i++) {
+            $arr = func_get_arg($i);
+
+            foreach ($isec as $k => & $v) {
+                if (!isset($arr[$k])) {
+                    unset($isec[$k]);
+                }
+            }
+        }
+        return $isec;
+    }
+}
 ?>
