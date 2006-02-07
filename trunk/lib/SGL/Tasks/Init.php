e3977fa176b9744f00ceddf38f72c34a59d3f636
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-02-07 22:07:50 +0000

    AppController cleanup

--- trunk/lib/SGL/Tasks/Init.php
+++ trunk/lib/SGL/Tasks/Init.php
@@ -428,4 +428,112 @@ class SGL_Task_EnsureBC extends SGL_Task
     }
 }
 
+    // +---------------------------------------+
+    // | Abstract classes                      |
+    // +---------------------------------------+
+
+/**
+ * Abstract request processor.
+ *
+ * @abstract
+ *
+ */
+class SGL_ProcessRequest
+{
+    function process(/*SGL_Output*/ $data) {}
+}
+
+/**
+ * Decorator.
+ *
+ * @abstract
+ */
+class SGL_DecorateProcess extends SGL_ProcessRequest
+{
+    var $processRequest;
+
+    function SGL_DecorateProcess(/* SGL_ProcessRequest */ $pr)
+    {
+        $this->processRequest = $pr;
+        $this->c = &SGL_Config::singleton();
+        $this->conf = $this->c->getAll();
+    }
+}
+
+/**
+ * Abstract renderer strategy
+ *
+ * @abstract
+ */
+class SGL_OutputRendererStrategy
+{
+    /**
+     * Prepare renderer options.
+     *
+     */
+    function initEngine() {}
+
+    /**
+     * Abstract render method.
+     *
+     * @param SGL_View $view
+     */
+    function render($view) {}
+}
+
+/**
+ * Container for output data and renderer strategy.
+ *
+ * @abstract
+ *
+ */
+class SGL_View
+{
+	/**
+	 * Output object.
+	 *
+	 * @var SGL_Output
+	 */
+	var $data;
+
+    /**
+     * Reference to renderer strategy.
+     *
+     * @var SGL_OutputRendererStrategy
+     */
+    var $rendererStrategy;
+
+    /**
+     * Constructor.
+     *
+     * @param SGL_Output $data
+     * @param SGL_OutputRendererStrategy $rendererStrategy
+     * @return SGL_View
+     */
+    function SGL_View($data, $rendererStrategy)
+    {
+    	$this->data = $data;
+    	$this->rendererStrategy = $rendererStrategy;
+    }
+
+    /**
+     * Post processing tasks specific to view type.
+     *
+     * @abstract
+     * @return boolean
+     */
+    function postProcess() {}
+
+
+    /**
+     * Delegates rendering strategy based on view.
+     *
+     * @return string   Rendered output data
+     */
+    function render()
+    {
+    	return $this->rendererStrategy->render($this);
+    }
+}
+
 ?>
