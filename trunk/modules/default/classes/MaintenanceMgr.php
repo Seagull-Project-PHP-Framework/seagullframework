d3c3477a5a5cd36df30afcf8712449660b269ba7
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-02-17 17:44:45 +0000

    Factored out module builder code into its own manager, ModuleGenerationMgr

--- trunk/modules/default/classes/MaintenanceMgr.php
+++ trunk/modules/default/classes/MaintenanceMgr.php
@@ -70,7 +70,6 @@ class MaintenanceMgr extends SGL_Manager
             'dbgen'     => array('dbgen'),
             'rebuildSequences' => array('rebuildSequences'),
             'clearCache' => array('clearCache'),
-            'createModule' => array('createModule', 'redirectToDefault'),
             'checkLatestVersion' => array('checkLatestVersion', 'redirectToDefault'),
             'list'      => array('list'),
         );
@@ -88,7 +87,6 @@ class MaintenanceMgr extends SGL_Manager
         $input->action      = ($req->get('action')) ? $req->get('action') : 'list';
         $input->aTranslation = $req->get('translation');
         $input->cache       = ($req->get('frmCache')) ? $req->get('frmCache') : array();
-        $input->createModule = (object)$req->get('frmCreateModule');
 
         //  get current module, check session
         $input->currentModule = $req->get('frmCurrentModule');
@@ -137,16 +135,6 @@ class MaintenanceMgr extends SGL_Manager
             }
             if ($input->action == 'clearCache' && !count($input->cache)) {
                 $aErrors['nothingChecked'] = SGL_Output::translate('please check at least one box');
-            }
-
-            //  checks for creating modules
-            if ($input->action == 'createModule') {
-                if (empty($input->createModule->moduleName)) {
-                    $aErrors['moduleName'] = SGL_Output::translate('please enter module name');
-                }
-                if (empty($input->createModule->managerName)) {
-                    $aErrors['managerName'] = SGL_Output::translate('please enter manager name');
-                }
             }
         }
         //  if errors have occured
@@ -516,167 +504,6 @@ class MaintenanceMgr extends SGL_Manager
             } else {
                 SGL::raiseMsg('Cache files successfully deleted');
             }
-        }
-    }
-
-    function _cmd_createModule(&$input, &$output)
-    {
-        SGL::logMessage(null, PEAR_LOG_DEBUG);
-
-        $modName = strtolower($input->createModule->moduleName);
-        $mgrName = ucfirst($input->createModule->managerName);
-        $aActions = '';
-
-        //  strip final 'mgr' if necessary
-        if (preg_match('/mgr$/i', $mgrName)) {
-            $origMgrName = $mgrName;
-            $mgrName = preg_replace("/mgr/i", '', $mgrName);
-        }
-        //  set mod/mgr details
-        $output->moduleName = $modName;
-
-        $output->managerName = $mgrName;
-        $mgrLongName = (isset($origMgrName))
-            ? $origMgrName
-            : ucfirst($input->createModule->managerName) . 'Mgr';
-
-        //  build template name
-        $firstLetter = $mgrLongName{0};
-        $restOfWord = substr($mgrLongName, 1);
-        $templatePrefix = strtolower($firstLetter).$restOfWord;
-        $output->templatePrefix = $templatePrefix;
-
-        //  set author details
-        require_once 'DB/DataObject.php';
-        $user = DB_DataObject::factory($this->conf['table']['user']);
-        $user->get(SGL_Session::getUid());
-        $output->authorName = $user->first_name . ' ' . $user->last_name;
-        $output->authorEmail = $user->email;
-
-        //  array: methodName => array (aActionsmapping string, templateName)
-        $aPossibleMethods = array(
-            'add'   => array("'add'       => array('add'),",'Add.html'),
-            'insert'=> array("'insert'    => array('insert', 'redirectToDefault'),"),
-            'edit'  => array("'edit'      => array('edit'), ",'Add.html'),
-            'update'=> array("'update'    => array('update', 'redirectToDefault'),"),
-            'list'  => array("'list'      => array('list'),",'List.html'),
-            'delete'=> array("'delete'    => array('delete', 'redirectToDefault'),"),
-        );
-        foreach ($aPossibleMethods as $method=>$mapping) {
-
-            //  if checked add to aMethods array
-            if (isset($input->createModule->$method)) {
-                $aMethods[] = $method;
-                $aActions[] = $mapping[0];
-                isset($mapping[1]) ? $aTemplates[] = $mapping[1] : '';
-            }
-        }
-        $methods = '';
-        if (isset($aMethods) && count($aMethods)) {
-            foreach ($aMethods as $method) {
-                if (isset($input->createModule->$method)) {
-                    $methods .= <<< EOF
-
-    function _cmd_$method(&\$input, &\$output)
-    {
-        SGL::logMessage(null, PEAR_LOG_DEBUG);
-    }
-
-EOF;
-                }
-            }
-        }
-
-        $output->methods = $methods;
-        $output->aActionMapping = $aActions;
-
-        //  initialise template engine
-        require_once 'HTML/Template/Flexy.php';
-        $options = &PEAR::getStaticProperty('HTML_Template_Flexy','options');
-        $options = array(
-            'templateDir'       => SGL_MOD_DIR . '/default/classes/',
-            'compileDir'        => SGL_TMP_DIR,
-            'forceCompile'      => 1,
-            'filters'           => array('SimpleTags','Mail'),
-            'compiler'          => 'Regex',
-            'flexyIgnore'       => 0,
-            'globals'           => true,
-            'globalfunctions'   => true,
-        );
-
-        $templ = & new HTML_Template_Flexy();
-        $templ->compile('ManagerTemplate.html');
-        $data = $templ->bufferedOutputObject($output, array());
-        $data = preg_replace("/\&amp;/s", '&', $data);
-        $mgrTemplate = "<?php\n" . $data . "\n?>";
-
-        //  setup directories
-        $aDirectories['module']     = SGL_MOD_DIR . '/' . $output->moduleName;
-        $aDirectories['classes']    = $aDirectories['module'] . '/classes';
-        $aDirectories['lang']       = $aDirectories['module'] . '/lang';
-        $aDirectories['templates']  = $aDirectories['module'] . '/templates';
-
-        if (is_writable(SGL_MOD_DIR)) {
-            require_once 'System.php';
-
-            foreach ($aDirectories as $directory){
-                //  pass path as array to avoid widows space parsing prob
-                $success = System::mkDir(array('-p', $directory));
-                //  attempt to get apache user to set 'other' bit as writable, so
-                //  you can edit this file
-                @chmod($directory, 0777);
-            }
-        } else {
-            SGL::raiseError('The modules directory does not appear to be writable, please give the
-                webserver permissions to write to it', SGL_ERROR_FILEUNWRITABLE);
-            return false;
-        }
-        //  write new manager to appropriate module
-        $targetMgrName = $aDirectories['classes'] . '/' . $output->managerName . 'Mgr.php';
-        $success = file_put_contents($targetMgrName, $mgrTemplate);
-
-        //  attempt to get apache user to set 'other' bit as writable, so
-        //  you can edit this file
-        @chmod($targetMgrName, 0666);
-        if (isset($input->createModule->createIniFile)){
-
-            //  create conf.ini
-            $confIniName = $aDirectories['module'] . '/conf.ini';
-            $confTemplate = '['.$mgrLongName.']'."\n";
-            $confTemplate .= 'requiresAuth    = false';
-            $success = file_put_contents($confIniName, $confTemplate);
-            @chmod($confIniName, 0666);
-        }
-        //  create language files
-        if (isset($input->createModule->createLangFiles)){
-            $fileTemplate = "<?php\n\$words=array();\n?>";
-            foreach ($GLOBALS['_SGL']['LANGUAGE'] as $language) {
-                $fileName = $aDirectories['module'] . '/lang/'.$language[1].'.php';
-                $success = file_put_contents($fileName, $fileTemplate);
-                @chmod($fileName, 0666);
-            }
-        }
-        //  create templates
-        if (isset($input->createModule->createTemplates)){
-            foreach($aTemplates as $template) {
-                $fileName = $aDirectories['templates'].'/'.$templatePrefix.$template;
-                $fileTemplate = 'Demo Template: '.$templatePrefix.$template;
-                $success = file_put_contents($fileName, $fileTemplate);
-                @chmod($fileName, 0666);
-            }
-        }
-
-        $shortTags = ini_get('short_open_tag');
-        $append = empty($shortTags)
-            ? ' However, you currently need to set "short_open_tag" to On for the templates to generate correctly.'
-            : '';
-
-
-        if (!$success) {
-            SGL::raiseError('There was a problem creating the files',
-                SGL_ERROR_FILEUNWRITABLE);
-        } else {
-            SGL::raiseMsg('Module files successfully created' . $append, false);
         }
     }
 
