336ea6ffa7d31d1863360e375e581676f3cac798
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-02-20 22:30:55 +0000

    updated block data with new format

--- trunk/modules/block/data/data.sample.my.sql
+++ trunk/modules/block/data/data.sample.my.sql
@@ -1,17 +1,17 @@
 #
 # Dumping data for table `block`
 #
-INSERT INTO `block` VALUES (1, 'SiteNews', 'Site News', '', '', 2, 'Left', 1, 1, 'N;');
-INSERT INTO `block` VALUES (3, 'SampleBlock1', 'SourceForge Site', '', '', 4, 'Left', 0, 1, 'N;');
-INSERT INTO `block` VALUES (4, 'SampleBlock2', 'Syndication', '', '', 3, 'Left', 0, 1, 'N;');
-INSERT INTO `block` VALUES (5, 'CategoryNav', 'Categories', '', 'navWidget', 1, 'AdminCategory', 1, 1, 'N;');
-INSERT INTO `block` VALUES (10, 'SampleRightBlock1', 'Sample Right Block', '', '', 5, 'Right', 1, 1, NULL);
-INSERT INTO `block` VALUES (11, 'CalendarBlock', 'Calendar', '', '', 2, 'Left', 0, 1, 'N;');
-INSERT INTO `block` VALUES (12, 'LoginBlock', 'Login', '', '', 1, 'Right', 1, 0, 'N;');
-INSERT INTO `block` VALUES (17, 'RecentHtmlArticles2', 'Recent articles', '', '', 3, 'Right', 1, 1, 'N;');
-INSERT INTO `block` VALUES (18, 'Navigation', 'User menu', '', '', 1, 'UserNav', 1, 1, 'a:6:{s:15:"startParentNode";s:1:"1";s:10:"startLevel";s:1:"0";s:14:"levelsToRender";s:1:"0";s:9:"collapsed";s:1:"1";s:10:"showAlways";s:1:"1";s:11:"breadcrumbs";s:1:"0";}');
-INSERT INTO `block` VALUES (19, 'Article', 'Articles statiques', '', '', 4, 'Right', 0, 1, 'a:2:{s:9:"articleId";s:1:"2";s:8:"template";s:33:"articleViewStaticHtmlArticle.html";}');
-INSERT INTO `block` VALUES (20, 'Navigation', 'Admin menu', '', '', 1, 'AdminNav', 1, 1, 'a:6:{s:15:"startParentNode";s:2:"22";s:10:"startLevel";s:1:"0";s:14:"levelsToRender";s:1:"0";s:9:"collapsed";s:1:"0";s:10:"showAlways";s:1:"1";s:11:"breadcrumbs";s:1:"0";}');
+INSERT INTO `block` VALUES (1, 'Publisher_Block_SiteNews', 'Site News', '', '', 2, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (3, 'Default_Block_Sample1', 'SourceForge Site', '', '', 4, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (4, 'Default_Block_Sample2', 'Syndication', '', '', 3, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (5, 'Navigation_Block_CategoryNav', 'Categories', '', 'navWidget', 1, 'AdminCategory', 0, 1, 'N;');
+INSERT INTO `block` VALUES (10, 'Default_Block_SampleRight1', 'Sample Right Block', '', '', 5, 'Right', 0, 1, 'N;');
+INSERT INTO `block` VALUES (11, 'Default_Block_Calendar', 'Calendar', '', '', 2, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (12, 'User_Block_Login', 'Login', '', '', 1, 'Right', 0, 0, 'N;');
+INSERT INTO `block` VALUES (17, 'Publisher_Block_RecentHtmlArticles2', 'Recent articles', '', '', 3, 'Right', 0, 1, 'N;');
+INSERT INTO `block` VALUES (18, 'Navigation_Block_Navigation', 'User menu', '', '', 1, 'UserNav', 0, 1, 'a:9:{s:15:"startParentNode";s:1:"1";s:10:"startLevel";s:1:"0";s:14:"levelsToRender";s:1:"0";s:9:"collapsed";s:1:"1";s:10:"showAlways";s:1:"1";s:12:"cacheEnabled";s:1:"1";s:11:"breadcrumbs";s:1:"0";s:8:"renderer";s:9:"SimpleNav";s:8:"template";s:0:"";}');
+INSERT INTO `block` VALUES (19, 'Publisher_Block_Article', 'Articles statiques', '', '', 4, 'Right', 0, 1, 'a:2:{s:9:"articleId";s:1:"2";s:8:"template";s:33:"articleViewStaticHtmlArticle.html";}');
+INSERT INTO `block` VALUES (20, 'Navigation_Block_Navigation', 'Admin menu', '', '', 1, 'AdminNav', 0, 1, 'a:9:{s:15:"startParentNode";s:2:"22";s:10:"startLevel";s:1:"0";s:14:"levelsToRender";s:1:"0";s:9:"collapsed";s:1:"0";s:10:"showAlways";s:1:"1";s:12:"cacheEnabled";s:1:"1";s:11:"breadcrumbs";s:1:"0";s:8:"renderer";s:9:"SimpleNav";s:8:"template";s:0:"";}');
 
 
 #
