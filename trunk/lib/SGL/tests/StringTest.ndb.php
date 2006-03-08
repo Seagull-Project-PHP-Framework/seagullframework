882c0d6b9d0d3e0499d6bacbbcba146d9241b614
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-03-08 18:38:58 +0000

    added some dirify tests

--- trunk/lib/SGL/tests/StringTest.ndb.php
+++ trunk/lib/SGL/tests/StringTest.ndb.php
@@ -7,49 +7,49 @@ require_once dirname(__FILE__) . '/../String.php';
  * @package SGL
  * @author  Demian Turner <demian@phpkitchen.net>
  * @version $Id: UrlTest.ndb.php,v 1.1 2005/06/23 14:56:01 demian Exp $
- */  
+ */
 class StringTest extends UnitTestCase {
 
     function StringTest()
     {
         $this->UnitTestCase('String Test');
     }
-    
+
     function testStripIniFileIllegalChars()
     {
         $target = 'these are legal chars';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)));
-        
+
         $target = 'contains illegal " character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
-        
+
         $target = 'contains illegal | character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
-        
+
         $target = 'contains illegal & character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
-        
+
         $target = 'contains illegal ~ character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
-        
+
         $target = 'contains illegal ! character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
-        
+
         $target = 'contains illegal ( character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
-        
+
         $target = 'contains illegal ) character';
         $targetLen = strlen($target);
         $this->assertEqual($targetLen, strlen(SGL_String::stripIniFileIllegalChars($target)) +1);
     }
-    
+
     function testRemoveEmptyElements()
     {
         $arr = array(
@@ -60,13 +60,27 @@ class StringTest extends UnitTestCase {
                 4 => '',
                 5 => array(),
                   );
-                  
+
         $target = array(
                 0 => 'foo',
                 2 => -1,
                 );
         $arr = SGL_Array::removeBlanks($arr);
         $this->assertEqual($arr, $target);
+    }
+
+    function testDirify()
+    {
+        $aControl[] = 'Here is a sentence-like string.';
+        $aControl[] = ' Here is a sentence-like string.';
+        $aControl[] = ' *Here is a sentence-like string.';
+        $aExpected[] = 'here_is_a_sentencelike_string';
+        $aExpected[] = '_here_is_a_sentencelike_string';
+        $aExpected[] = '_here_is_a_sentencelike_string';
+        foreach ($aControl as $k => $control) {
+            $ret = SGL_String::dirify($control);
+            $this->assertEqual($aExpected[$k], $ret);
+        }
     }
 }
 
