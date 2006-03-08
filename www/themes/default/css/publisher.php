535eb8c3af5c36a4c9e171c4a5dfa672c01ed78a
Demian Turner <demian@seagullsystems.com>
2006-03-08 02:14:38 +0000

    module-specific CSS moved out of default, fixes #681

---
+++ www/themes/default/css/publisher.php
@@ -0,0 +1,49 @@
+.sectionHeader {
+    font-size: 1.3em;
+    font-weight: normal;
+    color: <?php echo $sectionHeaderColor ?>;
+    background-color: <?php echo $sectionHeaderBackground ?>;
+    padding-left: 10px;
+    line-height: 34px;
+    letter-spacing: 1px;
+    margin: 0;
+}
+.colHeader {
+    color: <?php echo $colHeaderColor ?>;
+    background-color: <?php echo $colHeaderBackground ?>;
+    font-size: 11px;
+    line-height: 20px;
+    font-weight: normal;
+    padding-left: 10px;
+    letter-spacing: 0.5px;
+    margin: 2px 0 0 0;
+}
+.linkCrumbsAlt1 {
+    text-decoration: none;
+    color: <?php echo $secondaryDarker ?>;
+    font-weight: normal;
+    letter-spacing: 0.5px;
+}
+.linkCrumbsAlt1:hover {
+    text-decoration: underline;
+    color: <?php echo $secondaryDarker ?>;
+}
+
+/* /////////////// Article Manager /////////////// */
+
+.forApproval {
+    font-weight: bold;
+    color: <?php echo $forApproval ?>;
+}
+.approved {
+    font-weight: bold;
+    color: <?php echo $approved ?>;
+}
+.published {
+    font-weight: bold;
+    color: <?php echo $published ?>;
+}
+.archived {
+    font-weight: bold;
+    color:  <?php echo $archived ?>;
+}
