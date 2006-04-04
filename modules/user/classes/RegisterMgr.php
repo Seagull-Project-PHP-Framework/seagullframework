cc3a30fe5b4d9e05fc7c721e35e6951d358dc526
Demian Turner <demian@seagullsystems.com>
2006-04-04 10:52:20 +0000

    updated registration details

--- modules/user/classes/RegisterMgr.php
+++ modules/user/classes/RegisterMgr.php
@@ -279,7 +279,7 @@ class RegisterMgr extends SGL_Manager
                     'template'  => SGL_THEME_DIR . '/' . $_SESSION['aPrefs']['theme'] . '/' .
                         $moduleName . '/email_registration_admin.php',
                     'username'      => $oUser->username,
-                    'activationUrl'      => 'http://seagull.phpkitchen.com/index.php/user/',
+                    'activationUrl'      => 'http://seagullproject.org/register/',
             );
             $notification = & new SGL_Emailer($options);
             $notification->prepare();
