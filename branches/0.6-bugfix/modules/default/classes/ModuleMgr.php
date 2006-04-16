4266e2144066f53bfbc5389ec1c93b5666fe0a59
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-04-16 18:51:44 +0000

    consolidated duplicated resources

--- branches/0.6-bugfix/modules/default/classes/ModuleMgr.php
+++ branches/0.6-bugfix/modules/default/classes/ModuleMgr.php
@@ -1,7 +1,7 @@
 <?php
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Copyright (c) 2005, Demian Turner                                         |
+// | Copyright (c) 2006, Demian Turner                                         |
 // | All rights reserved.                                                      |
 // |                                                                           |
 // | Redistribution and use in source and binary forms, with or without        |
@@ -30,7 +30,7 @@
 // | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.      |
 // |                                                                           |
 // +---------------------------------------------------------------------------+
-// | Seagull 0.5                                                               |
+// | Seagull 0.6                                                               |
 // +---------------------------------------------------------------------------+
 // | ModuleMgr.php                                                             |
 // +---------------------------------------------------------------------------+
@@ -197,6 +197,10 @@ class ModuleMgr extends SGL_Manager
         $output->pageTitle = 'Module Manager :: Add';
         $output->action = 'insert';
         $output->template  = 'moduleEdit.html';
+
+        $module = new stdClass();
+        $module->icon = "48/module_default.png";
+        $output->module = $module;
     }
 
     function _cmd_insert(&$input, &$output)
