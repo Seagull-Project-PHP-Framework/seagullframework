bb7d65e01b4e358ce9d9999248ba3b34345357bb
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-04-19 11:28:27 +0000

    added sgl rss as sample block

--- branches/0.6-bugfix/modules/block/data/data.sample.my.sql
+++ branches/0.6-bugfix/modules/block/data/data.sample.my.sql
@@ -5,14 +5,15 @@
 #INSERT INTO `block` VALUES (1, 'Navigation_Block_Navigation', 'Admin menu', '', '', 1, 'AdminNav', 1, 1, 'a:9:{s:15:"startParentNode";s:1:"4";s:10:"startLevel";s:1:"0";s:14:"levelsToRender";s:1:"0";s:9:"collapsed";s:1:"1";s:10:"showAlways";s:1:"1";s:12:"cacheEnabled";s:1:"1";s:11:"breadcrumbs";s:1:"0";s:8:"renderer";s:14:"SimpleRenderer";s:8:"template";s:0:"";}');
 #INSERT INTO `block` VALUES (2, 'Navigation_Block_Navigation', 'User menu', '', '', 1, 'UserNav', 1, 1, 'a:9:{s:15:"startParentNode";s:1:"2";s:10:"startLevel";s:1:"0";s:14:"levelsToRender";s:1:"0";s:9:"collapsed";s:1:"1";s:10:"showAlways";s:1:"1";s:12:"cacheEnabled";s:1:"1";s:11:"breadcrumbs";s:1:"0";s:8:"renderer";s:14:"SimpleRenderer";s:8:"template";s:0:"";}');
 INSERT INTO `block` VALUES (3, 'Navigation_Block_CategoryNav', 'Categories', '', 'navWidget', 1, 'AdminCategory', 0, 1, 'N;');
-INSERT INTO `block` VALUES (4, 'User_Block_Login', 'Login', '', '', 5, 'Left', 1, 0, 'N;');
+INSERT INTO `block` VALUES (4, 'User_Block_Login', 'Login', '', '', 5, 'Right', 1, 0, 'N;');
 INSERT INTO `block` VALUES (5, 'Default_Block_SampleRight1', 'Sample Right Block', '', '', 5, 'Right', 1, 1, 'N;');
-INSERT INTO `block` VALUES (6, 'Publisher_Block_SiteNews', 'Site News', '', '', 1, 'Left', 0, 1, 'N;');
-INSERT INTO `block` VALUES (7, 'Default_Block_Sample1', 'Community', '', '', 4, 'Left', 1, 1, 'N;');
-INSERT INTO `block` VALUES (8, 'Default_Block_Sample2', 'Syndication', '', '', 3, 'Left', 0, 1, 'N;');
-INSERT INTO `block` VALUES (9, 'Default_Block_Calendar', 'Calendar', '', '', 2, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (6, 'Publisher_Block_SiteNews', 'Site News', '', '', 3, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (7, 'Default_Block_Sample1', 'Community', '', '', 1, 'Left', 1, 1, 'N;');
+INSERT INTO `block` VALUES (8, 'Default_Block_Sample2', 'Syndication', '', '', 5, 'Left', 0, 1, 'N;');
+INSERT INTO `block` VALUES (9, 'Default_Block_Calendar', 'Calendar', '', '', 4, 'Left', 0, 1, 'N;');
 INSERT INTO `block` VALUES (10, 'Publisher_Block_RecentHtmlArticles2', 'Recent articles', '', '', 3, 'Right', 0, 1, 'N;');
 INSERT INTO `block` VALUES (11, 'User_Block_OnlineUsers', 'Online', '', '', 6, 'Left', 0, 0, 'N;');
+INSERT INTO `block` VALUES (12, 'Export_Block_SampleRss', 'Latest Seagull News', '', '', 2, 'Left', 1, 1, 'N;');
 
 --
 -- Dumping data for table `block_assignment`
@@ -29,6 +30,8 @@ INSERT INTO `block_assignment` VALUES (8, 6);
 INSERT INTO `block_assignment` VALUES (9, 6);
 INSERT INTO `block_assignment` VALUES (10, 6);
 INSERT INTO `block_assignment` VALUES (11, 6);
+INSERT INTO `block_assignment` VALUES (12, 0);
+
 
 --
 -- Dumping data for table `block_role`
@@ -45,3 +48,4 @@ INSERT INTO `block_role` VALUES (8, -2);
 INSERT INTO `block_role` VALUES (9, -2);
 INSERT INTO `block_role` VALUES (10, -2);
 INSERT INTO `block_role` VALUES (11, -2);
+INSERT INTO `block_role` VALUES (12, -2);
