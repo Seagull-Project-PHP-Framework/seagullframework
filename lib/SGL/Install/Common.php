8be72620e0a532574c16f77c795ce5e00c334f10
Demian Turner <demian@seagullsystems.com>
2006-05-03 11:31:50 +0000

    Fixed cmdline installer

--- lib/SGL/Install/Common.php
+++ lib/SGL/Install/Common.php
@@ -50,6 +50,9 @@ class SGL_Install_Common
         if (!isset($GLOBALS['_SGL'])) {
             $GLOBALS['_SGL'] = array();
         }
+        if (!isset($_SESSION['ERRORS'])) {
+            $_SESSION['ERRORS'] = array();
+        }
         array_push($_SESSION['ERRORS'], $error);
     }
 
