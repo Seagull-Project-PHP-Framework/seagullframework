08a3cbb167f4a63546b7214eb59315f209e30f86
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-05-15 00:00:35 +0000

    adding new theme

---
+++ branches/0.6-bugfix/www/themes/classic/css/vars.php
@@ -0,0 +1,59 @@
+<?php
+    $fontFamily             = 'verdana, sans-serif';
+    $fontFamilyAlt          = 'arial';
+    $fontSize               = 'small';
+
+    $primary                = '#99CC00'; // lime green
+    $primaryDark            = '#66A326'; //
+    $primaryLight           = '#EEF7D4'; //
+
+    $secondary              = '#0099CC'; // blue
+    $secondaryDark          = '#2666A3'; //
+    $secondaryLight         = '#E5F8FF'; //
+
+    $tertiary               = '#CC0099'; // cyan
+    $tertiaryDark           = '#A32666'; //
+    $tertiaryLight          = '#FFE5F8'; //
+
+    $grey                   = '#BBBBBB'; // grey
+    $greyDark               = '#666666'; //
+    $greyDarkest            = '#000000'; //
+    $greyLight              = '#EEEEEE'; //
+    $greyLightest           = '#FFFFFF'; //
+
+/* Columns
+    ------------------------------------------------- */
+    $leftColWidth           = '15em';
+    $rightColWidth          = '15em';
+
+/* Messages and errors
+    ------------------------------------------------- */
+    $error                  = '#FF3300'; //
+
+/* Links related vars
+    ------------------------------------------------- */
+    $linkColor              = $secondary;
+    $linkDecoration         = 'none';
+    $linkHoverColor         = $secondary;
+    $linkHoverDecoration    = 'underline';
+
+/* Borders related vars
+    ------------------------------------------------- */
+    $borderDark             = $greyDark;
+    $borderLight            = $greyLightest;
+
+/* Block related vars
+    ------------------------------------------------- */
+    $blocksBorderBody       = $greyLightest;
+    $blocksBorderTitle      = $greyLightest;
+    $blocksBackgroundBody   = $greyLightest; // not used yet
+    $blocksBackgroundTitle  = $greyLightest;
+    $blocksColorBody        = $greyLightest; // ^ ^ ^
+    $blocksColorTitle       = $greyLightest; // | | |
+    
+/* Table related vars
+    ------------------------------------------------- */
+    $tableRowLight          = $greyLightest;
+    $tableRowDark           = $grey;
+
+?>
