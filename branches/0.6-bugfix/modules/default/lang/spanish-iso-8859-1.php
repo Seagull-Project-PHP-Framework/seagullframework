770a9cd1e217531c8f78286bd631a18609602c64
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-05-15 01:26:44 +0000

    applied spanish lang patch, fixes #922

--- branches/0.6-bugfix/modules/default/lang/spanish-iso-8859-1.php
+++ branches/0.6-bugfix/modules/default/lang/spanish-iso-8859-1.php
@@ -1,4 +1,28 @@
-<?php
+﻿<?php
+
+    $defaultWords = array(
+        'Maintenance' => 'Mantenimiento',
+        'Congratulations, the target translation appears to be up to date' => 'Enhorabuena, la traducci�n parece que est� al d�a',
+        'translation successfully updated' => 'traducci�n actualizada correctamente',
+        'There was a problem updating the translation' => 'Apareci� un problema cuando se trataba de actualizar la traducci�n',
+        'Data Objects rebuilt successfully' => 'Los Objetos de Datos han sido reconstruidos correctamente',
+        'Cache files successfully deleted' => 'Los Fichero de Cach� han sido borrados',
+        'Manage Translations' => 'Gestionar traducciones',
+        'Sequences rebuilt successfully' => 'Las secuencias han sido reconstruidas correctamente',
+        'Rebuild DB Sequences' => 'Las secuencias de las BD ha sido reconstruidas',
+        'Rebuild Sequences Now' => 'Reconstruir secuencias ahora',
+		'validate' => 'validar',
+        'edit' => 'editar',
+        'Process' => 'Precesar',
+        'Manage Caches' => 'Gestionar Cach�s',
+        'Templates' => 'Plantillas',
+        'Navigation & Blocks' => 'Navegaci�n & Bloques',
+        'Templates' => 'Plantillas',
+        'Clear Selected Caches Now' => 'Limpiar las Cach�s Seleccionadas Ahora',
+        'Rebuild Data Objects' => 'Recontruir los Objetos del Datos',
+        'Rebuild Dataobjects Now' => 'Recosntruir los Objetos de Datos Ahora',
+    );
+
 $defaultWords['Welcome to'] = 'Bienvenido a';
 $defaultWords['Home'] = 'P�gina principal';
 $defaultWords['Module Manager'] = 'Gestor de M�dulos';
@@ -99,26 +123,4 @@ $defaultWords['Please, specify a description'] = 'Por favor, indique una descrip
 $defaultWords['Please, specify the url to link to'] = 'Por favor, indique una direcci�n a la que referirse';
 $defaultWords['Please, specify the name of the icon-file'] = 'Por favor, indique el nombre del fichero del icono';
 
-$defaultWords = array(
-        'Maintenance' => 'Mantenimiento',
-        'Congratulations, the target translation appears to be up to date' => 'Enhorabuena, la traducci�n parece que est� al d�a',
-        'translation successfully updated' => 'traducci�n actualizada correctamente',
-        'There was a problem updating the translation' => 'Apareci� un problema cuando se trataba de actualizar la traducci�n',
-        'Data Objects rebuilt successfully' => 'Los Objetos de Datos han sido reconstruidos correctamente',
-        'Cache files successfully deleted' => 'Los Fichero de Cach� han sido borrados',
-        'Manage Translations' => 'Gestionar traducciones',
-        'Sequences rebuilt successfully' => 'Las secuencias han sido reconstruidas correctamente',
-        'Rebuild DB Sequences' => 'Las secuencias de las BD ha sido reconstruidas',
-        'Rebuild Sequences Now' => 'Reconstruir secuencias ahora',
-		'validate' => 'validar',
-        'edit' => 'editar',
-        'Process' => 'Precesar',
-        'Manage Caches' => 'Gestionar Cach�s',
-        'Templates' => 'Plantillas',
-        'Navigation & Blocks' => 'Navegaci�n & Bloques',
-        'Templates' => 'Plantillas',
-        'Clear Selected Caches Now' => 'Limpiar las Cach�s Seleccionadas Ahora',
-        'Rebuild Data Objects' => 'Recontruir los Objetos del Datos',
-        'Rebuild Dataobjects Now' => 'Recosntruir los Objetos de Datos Ahora',
-        );
 ?>
