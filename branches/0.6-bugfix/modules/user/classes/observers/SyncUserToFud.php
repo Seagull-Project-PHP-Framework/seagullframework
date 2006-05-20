36257c7c9fc73cc680e450329f2ee99485913148
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-05-20 21:30:53 +0000

    added email to fud record

--- branches/0.6-bugfix/modules/user/classes/observers/SyncUserToFud.php
+++ branches/0.6-bugfix/modules/user/classes/observers/SyncUserToFud.php
@@ -8,16 +8,18 @@ class SyncUserToFud extends SGL_Observer
         }
         $dsn1 = 'mysql://root@localhost/seagull_live';
         $dbh1 = & SGL_DB::singleton($dsn1);
-        //  get max ID
+        //  get max ID and email
         $userId = $dbh1->getOne("SELECT MAX(usr_id) FROM usr");
+        $email = $dbh1->getOne("SELECT email FROM usr WHERE usr_id = $userId");
 
         //  insert into FUD
         $dsn2 = 'mysql://root@localhost/fud';
         $dbh2 = & SGL_DB::singleton($dsn2);
         $username = $dbh2->quoteSmart($observable->input->user->username);
+        $email = $dbh2->quoteSmart($email);
         $query = "
-            INSERT INTO `fud26_users` (`id`, `login`, `alias`, `theme`, `users_opt`)
-            VALUES ($userId, $username, $username, 1, 4357110)";
+            INSERT INTO `fud26_users` (`id`, `login`, `alias`, `email`, theme`, `users_opt`)
+            VALUES ($userId, $username, $username, $email, 1, 4357110)";
         $ok = $dbh2->query($query);
         return $ok;
     }
