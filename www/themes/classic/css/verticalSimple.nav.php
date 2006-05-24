a384c878f20865fbd4deadbd2bcc89406e1159a5
Demian Turner <demian@seagullsystems.com>
2006-05-24 23:35:43 +0000

    applied vertical nav patch, fixes #965

--- www/themes/classic/css/verticalSimple.nav.php
+++ www/themes/classic/css/verticalSimple.nav.php
@@ -1,12 +1,12 @@
 /* first level list */
-div#sgl-nav ul {
+div#nav ul {
 	list-style: none;
 	padding: 0px;
 	margin: 0px;
 }
 
 /* first level list item */
-div#sgl-nav li {
+div#nav li {
 	margin: 3px 0 3px 0;
 	padding: 3px 0 3px 0;
 	background: #9dcdfe;
@@ -15,7 +15,7 @@ div#sgl-nav li {
 }
 
 /* second level list */
-div#sgl-nav ul ul {
+div#nav ul ul {
 	list-style: disc inside;
 	background: #9dcdfe;
 	padding: 3px 0 0 0;
@@ -23,41 +23,41 @@ div#sgl-nav ul ul {
 }
 
 /* second level list item */
-#sgl-nav li li {
+#nav li li {
 	text-transform: none;
 	margin: 0 0 3px 0;
 	padding: 3px 0 3px 5px;
 }
 
 /* menu item link */
-div#sgl-nav a:link,
-div#sgl-nav a:visited,
-div#sgl-nav a:hover,
-div#sgl-nav a:active,
-div#sgl-nav li.current li a:link,
-div#sgl-nav li.current li a:visited,
-div#sgl-nav li.current li a:active {
+div#nav a:link,
+div#nav a:visited,
+div#nav a:hover,
+div#nav a:active,
+div#nav li.current li a:link,
+div#nav li.current li a:visited,
+div#nav li.current li a:active {
 	color: #006699;
 	font-size: 11px;
     font-weight: normal;
     text-decoration: none;
 	padding: 0 5px 0 5px;
 }
-div#sgl-nav li a:hover {
+div#nav li a:hover {
     color: #ffffff;
 }
-div#sgl-nav a:active,
-div#sgl-nav li.current li a:active {
+div#nav a:active,
+div#nav li.current li a:active {
 	background: #9dcdfe;
 }
 
 /* current menu item link */
-div#sgl-nav li.current a:link,
-div#sgl-nav li.current a:visited,
-div#sgl-nav li.current a:active {
+div#nav li.current a:link,
+div#nav li.current a:visited,
+div#nav li.current a:active {
 	color: #006699;
     font-weight: bold;
 }
-div#sgl-nav li.current a:active {
+div#nav li.current a:active {
 	background: #ffecec;
 }
