064140a20721edd61d34529ae63d6d5636dac38e
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-05-27 15:26:28 +0000

    fixed unit tests

--- branches/0.6-bugfix/lib/SGL/UrlParser/AliasStrategy.php
+++ branches/0.6-bugfix/lib/SGL/UrlParser/AliasStrategy.php
@@ -8,6 +8,7 @@
  */
 
 require_once SGL_MOD_DIR . '/navigation/classes/DA_Navigation.php';
+require_once SGL_CORE_DIR . '/UrlParser/SimpleStrategy.php';
 
 /**
  * Concrete alias url parser strategy
