dfdb8b4e2d84907eb7aa0825704e5826bd4a2ce6
Demian Turner <demian@seagullsystems.com>
2006-05-27 15:26:28 +0000

    fixed unit tests

--- modules/user/tests/LoginMgrTest.wdd.php
+++ modules/user/tests/LoginMgrTest.wdd.php
@@ -47,8 +47,13 @@ class TestLoginMgr extends UnitTestCase {
         $userMgr->_cmd_insert($oInput, $oOutput);
 
         //  test login with new user details
-        $loginMgr = new LoginMgr();
-        $res = $loginMgr->_doLogin($username, $passwd);
+        $oInput = new stdClass();
+        $oOutput = new stdClass();
+        $doLogin = new User_DoLogin($oInput, $oOutput);
+        $doLogin->dbh = $doLogin->_getDb();
+        $c = &SGL_Config::singleton();
+        $doLogin->conf = $c->getAll();
+        $res = $doLogin->_doLogin($username, $passwd);
         $this->assertTrue(is_array($res));
         $this->assertTrue(count($res));
     }
