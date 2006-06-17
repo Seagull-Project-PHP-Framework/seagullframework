58eecb97572c1b1b76bf44f7d45a5af3d9391f51
aj <aj@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-06-17 11:22:37 +0000

    created article alias branch

---
+++ branches/articlealias/modules/default/lang/portuguese-iso-8859-1.php
@@ -0,0 +1,152 @@
+<?php
+    $defaultWords = array(
+/*  HOME    */
+        'Welcome to' => 'Bem Vindo ao',
+        'Home' => 'Inicio',
+
+/*  MODULE MGR  */
+        'Module Manager' => 'Gestor de M�dulos',
+
+/*  CONFIG MGR  */
+        'Config Manager' => 'Gestor de Configura��es',
+        'config info successfully updated' => 'A configura��o foi gravada com sucesso',
+
+/*  FOOTER  */
+        'Execution Time' => 'Tempo de execu��o',
+        'Powered by' => 'Powered by',
+
+/*  GENERAL MESSAGES    */
+        'insufficient rights' => 'N�o tem permiss�es suficientes para entrar nesta �rea.',
+        'authorization required' => '� necess�rio ser um utilizador registado para usar esta funcionalidade. <br>Por favor, use o formul�rio respectivo para se registar.',
+        'session timeout' => 'A sess�o expirou. Entre novamente.',
+        'You have been successfully logged out' => 'Saiu da sess�o',
+        'password emailed out' => 'A sua password foi enviada para o e-mail com que se registou',
+        'email not in system' => 'O seu e-mail n�o se encontra no sistema. Por favor tente outra vez.',
+        'email submitted successfully' => 'O seu email foi submetido.',
+        'There was a problem sending the email' => 'Ocorreu um erro no envio do seu e-mail.',
+        'message ID not recognised' => 'o ID da mensagem n�o � reconhecido',
+        'Please fill in the indicated fields' => 'Por favor preencha todos os campos abaixo indicados.',
+        'Your alert has been sent successfully' => 'A sua alerta foi enviado com sucesso',
+
+/*  VARIOUS */
+        'user' => 'utilizdor',
+        'Username' => 'Nome do Utilizador',
+        'Action' => 'Ac��o',
+        'Select' => 'Selecionar',
+        'delete' => 'remover',
+        'Edit' => 'Editar',
+        'View' => 'Visualizar',
+        'move up' => 'para cima',
+        'move down' => 'para baixo',
+        'finished' => 'finalizado',
+        'back to top' => 'voltar ao inicio',
+        'currently_logged_on_as' => 'utilizador',
+        'guest' => 'visitante',
+        'login' => 'login',
+        'logout' => 'logout',
+        'session started at' => 'sess�o iniciada �s',
+        'logged in at' => 'entrou �s',
+        'displaying results' => 'mostrar resultados',
+        'to' => 'at�',
+        'from a total of' => 'de um total de',
+        'back' => 'voltar',
+        'next' => 'seguinte',
+        'Send it' => 'Enviar',
+        'Submit' => 'Submeter',
+        'Cancel' => 'Cancelar',
+        'Reset' => 'Reset',
+        'reset' => 'reset',
+        'Save' => 'Gravar',
+        'add' => 'adicionar',
+        'edit' => 'editar',
+        'move' => 'mover',
+        'delete' => 'remover',
+        'Manage' => 'Gerir',
+        'Title'  => 'Tit�lo',
+        'Status' => 'Status',
+        'ID' => 'ID',
+        'Name' => 'Nome',
+
+        'check to activate' => 'clicar para activar',
+
+// Status
+        'Enabled' => 'Activo',
+        'Disabled' => 'Inactivo',
+
+
+        'Are you sure you want to delete this' => 'Confirma a remo��o?',
+        'You must select an element to delete' => 'Deve selecionadr um elemento para remover',
+        'no results found' => 'nenhum resultado encontrado',
+        'You have been banned' => 'Foi banido deste site. Contacte o respons�vel pelo mesmo para mais informa��es',
+        'Invalid POST source' => 'O formul�rio aparentemente foi enviado de uma fonte n�o autorizada',
+        'You are here' => 'Est� aqui',
+        'whats this?' => 'O que � isto?',
+        'denotes required field' => 'campo obrigat�rio',
+
+/*  Date and Time  */
+/*  'at time' used at Output:showDateSelector  */
+        'at time' => '�s',
+
+        'aMonths' => array(
+            13 => 'Janeiro',
+            14 => 'Fevereiro',
+            15 => 'Mar�o',
+            16 => 'Abril',
+            17 => 'Maio',
+            18 => 'Junho',
+            19 => 'Julho',
+            20 => 'Agosto',
+            21 => 'Setembro',
+            22 => 'Outubro',
+            23 => 'Novembro',
+            24 => 'Dezembro'
+        ),
+/*
+  Author: Michael willemot <michael@sotto.be>
+*/
+        'Manage' => 'Gerir',
+        'Return to browse' => 'Retornar ao browser',
+        'ModuleManager Mgr' => 'Gestor de M�dulos',
+        'Add' => 'Adicionar',
+        'Delete' => 'Remover',
+        'With selected module(s)' => 'Com o(s) m�dulo(s) selecionado(s)', //table footer
+        'Add a module' => 'Adicionar modulo',
+        'Module successfully added to the manager.' => 'Modulo adicionado com sucesos.',
+        'Module(s) successfully removed.' => 'Module(s) removido(s) com successo.',
+        'Configurable' => 'Configuravel',
+        'Description' => 'Descri��o',
+        'Admin URI' => 'Admin URI',
+        'Icon' => 'Icone',
+
+        // validation
+        'Please, specify a name' => 'Por favor indique um nome',
+        'Please, specify a title' => 'Por favor indique um titulo',
+        'Please, specify a description' => 'Por favor indique a descri��o',
+        'Please, specify the url to link to' => 'Por favor indique o url de liga��o',
+        'Please, specify the name of the icon-file' => 'Por favor indique o nome do ficheiro com o icone',
+
+        'Maintenance' => 'Manuten��o',
+        'Congratulations, the target translation appears to be up to date' => 'Parabens, a tradu��o esta actualizada',
+        'translation successfully updated' => 'tradu��o actualizada com sucesso',
+        'There was a problem updating the translation' => 'Ocorreu um problema na actualiza��o da tradu��o',
+        'Data Objects rebuilt successfully' => 'Data Objects foram reconstruidos com sucesso',
+        'Cache files successfully deleted' => 'Os ficheiso em Cache foram removidos com sucesso',
+        'Sequences rebuilt successfully' => 'A tabela de sequencias foi reconstruida com sucesso',
+
+        'Manage Translations' => 'Gerir tradu��es',
+        'Rebuild DB Sequences' => 'Reconstruir sequencias BD',
+        'Rebuild Sequences Now' => 'Executar',
+		'validate' => 'validar',
+        'edit' => 'editar',
+        'Process' => 'Processar',
+        'Manage Caches' => 'Gest�o da Cache',
+        'Templates' => 'Templates',
+        'Navigation & Blocks' => 'Navega��o e Blocos',
+
+        'Templates' => 'Templates',
+
+        'Clear Selected Caches Now' => 'Limpar Cache selecionada',
+        'Rebuild Data Objects' => 'Reconstruir Data Objects',
+        'Rebuild Dataobjects Now' => 'Executar',
+    );
+?>
---
+++ branches/articlealias/modules/export/blocks/SampleRss.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * A block to dislay an RSS feed.
+ *
+ * @package export
+ * @author  Demian Turner <demian@phpkitchen.com>
+ */
+
+define('SGL_RSS_ITEMS_TO_SHOW', 5);
+
+class Export_Block_SampleRss
+{
+    var $rssSource = 'http://seagullproject.org/export/rss/';
+
+    function init()
+    {
+        return $this->getBlockContent();
+    }
+
+    function getBlockContent()
+    {
+        if (ini_get('safe_mode')) {
+            return 'Cannot request remote feed in safe_mode ;-(';
+        }
+        $c = &SGL_Config::singleton();
+        $conf = $c->getAll();
+
+        $cache = & SGL_Cache::singleton($force = true);
+        if ($data = $cache->get('sglSiteRss', 'blocks')) {
+            $html = unserialize($data);
+            SGL::logMessage('rss from cache', PEAR_LOG_DEBUG);
+        } else {
+            require_once "XML/RSS.php";
+            $rss =& new XML_RSS($this->rssSource);
+            $rss->parse();
+
+            $html = "<ul class='noindent'>\n";
+            $x = 0;
+            foreach ($rss->getItems() as $item) {
+                $html .= "<li><a href=\"" . $item['link'] . "\">" . $item['title'] . "</a></li>\n";
+                $x ++;
+                if ($x > SGL_RSS_ITEMS_TO_SHOW) {
+                    break;
+                }
+            }
+            $html .= "</ul>\n";
+            $cache->save(serialize($html), 'sglSiteRss', 'blocks');
+            SGL::logMessage('rss from remote', PEAR_LOG_DEBUG);
+        }
+        return $html;
+    }
+}
+?>
