4efa65b0f86419fab297877d15b276d711e3c015
aj <aj@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-06-17 11:17:07 +0000

    imported 0.6.0RC3 into article alias branch

---
+++ branches/articlealias/0.6.0RC3/modules/guestbook/lang/english-iso-8859-15.php
@@ -0,0 +1,14 @@
+<?php
+    $words = array(
+        'Guestbook Manager' => 'Guestbook Manager',
+        'Guestbook Manager :: Add' => 'Guestbook Manager :: Add',
+        'No Guestbook entries found' => 'No Guestbook entries found',
+        'add new entry' => 'add new entry',
+        'Message' => 'Message',
+        'Please, specify your name' => 'Please, specify your name',
+        'Please, specify your email' => 'Please, specify your email',
+        'Please, fill in the message text' => 'Please, fill in the message text',
+        'Welcome to our Guestbook' => 'Welcome to our Guestbook',
+        'new guestbook entry saved successfully' => 'new guestbook entry saved successfully'
+    );
+?>
---
+++ branches/articlealias/0.6.0RC3/modules/randommsg/lang/italian-iso-8859-1.php
@@ -0,0 +1,22 @@
+<?php
+$words['RndMsg Manager :: Add'] = 'Gestore RndMsg :: Aggiungi';
+$words['RndMsg Manager :: Browse'] = 'Gestore RndMsg :: Elenco';
+$words['Default'] = 'Predefinito';
+$words['Message'] = 'Messaggio';
+$words['Delete'] = 'Elimina';
+$words['With selected message(s)'] = 'Con i messaggi selezionati';
+$words['Add messages'] = 'Aggiungi messaggi';
+$words['One or more messages successfully added.'] = 'Messaggi aggiunti';
+$words['Failed adding one ore more messages.'] = 'Errore durante l\'inserimento dei messaggi';
+$words['Message(s) successfully removed.'] = 'Messaggi eliminati';
+$words['An error occurred while deleting message(s).'] = 'Errore nella eliminazione dei messaggi';
+$words['Please enter one or more messages.'] = 'Inserire uno o più messaggi';
+$words['No message(s) selected.'] = 'Nessun messaggio selezionato';
+$words['add_message_intro'] = 'Inserisci i messaggi, uno per linea, andando a capo verranno creati due differenti messaggi';
+$words['add_from_file'] = 'Upload di un file di messaggi:';
+$words['You must select a file to upload'] = 'Manca il file per l\'upload';
+$words['Please wait until upload is finished'] = 'Attendere prego...upload in corso';
+$words['Upload'] = 'Upload';
+$words['Error: Not a recognised file type'] = 'Errore: tipo di file non riconosciuto (manca l\'estensione?)';
+$words['Please select a file to upload.'] = 'Selezionare un file per l\'upload';
+?>
