ee64ac8ae67181fa37955480afa2543ee65705fd
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-06-17 09:45:22 +0000

    applied polish encoding calendar patch, fixes #1016

--- branches/0.6-bugfix/www/js/jscalendar/lang/calendar-pl.js
+++ branches/0.6-bugfix/www/js/jscalendar/lang/calendar-pl.js
@@ -2,18 +2,20 @@
 // Calendar PL language
 // Author: Artur Filipiak, <imagen@poczta.fm>
 // January, 2004
-// Encoding: UTF-8
+// Update: Tomasz Osmialowski <ichtis@gmail.com>
+// July, 2005
+// Encoding: iso-8859-2
 Calendar._DN = new Array
-("Niedziela", "PoniedziaÅ‚ek", "Wtorek", "Åšroda", "Czwartek", "PiÄ…tek", "Sobota", "Niedziela");
+("Niedziela", "Poniedzia³ek", "Wtorek", "¦roda", "Czwartek", "Pi±tek", "Sobota", "Niedziela");
 
 Calendar._SDN = new Array
-("N", "Pn", "Wt", "Åšr", "Cz", "Pt", "So", "N");
+("N", "Pn", "Wt", "¦r", "Cz", "Pt", "So", "N");
 
 Calendar._MN = new Array
-("StyczeÅ„", "Luty", "Marzec", "KwiecieÅ„", "Maj", "Czerwiec", "Lipiec", "SierpieÅ„", "WrzesieÅ„", "PaÅºdziernik", "Listopad", "GrudzieÅ„");
+("Styczeñ", "Luty", "Marzec", "Kwiecieñ", "Maj", "Czerwiec", "Lipiec", "Sierpieñ", "Wrzesieñ", "Pa¼dziernik", "Listopad", "Grudzieñ");
 
 Calendar._SMN = new Array
-("Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "PaÅº", "Lis", "Gru");
+("Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Pa¼", "Lis", "Gru");
 
 // tooltips
 Calendar._TT = {};
@@ -25,32 +27,41 @@ Calendar._TT["ABOUT"] =
 "For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
 "Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
 "\n\n" +
-"WybÃ³r daty:\n" +
-"- aby wybraÄ‡ rok uÅ¼yj przyciskÃ³w \xab, \xbb\n" +
-"- aby wybraÄ‡ miesiÄ…c uÅ¼yj przyciskÃ³w " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + "\n" +
-"- aby przyspieszyÄ‡ wybÃ³r przytrzymaj wciÅ›niÄ™ty przycisk myszy nad ww. przyciskami.";
+"Wybór daty:\n" +
+"- aby wybraæ rok u¿yj przycisków \xab, \xbb\n" +
+"- aby wybraæ miesi±c u¿yj przycisków " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + "\n" +
+"- aby przyspieszyæ wybór przytrzymaj wci¶niêty przycisk myszy nad ww. przyciskami.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"WybÃ³r czasu:\n" +
-"- aby zwiÄ™kszyÄ‡ wartoÅ›Ä‡ kliknij na dowolnym elemencie selekcji czasu\n" +
-"- aby zmniejszyÄ‡ wartoÅ›Ä‡ uÅ¼yj dodatkowo klawisza Shift\n" +
-"- moÅ¼esz rÃ³wnieÅ¼ poruszaÄ‡ myszkÄ™ w lewo i prawo wraz z wciÅ›niÄ™tym lewym klawiszem.";
+"Wybór czasu:\n" +
+"- aby zwiêkszyæ warto¶æ kliknij na dowolnym elemencie selekcji czasu\n" +
+"- aby zmniejszyæ warto¶æ u¿yj dodatkowo klawisza Shift\n" +
+"- mo¿esz równie¿ poruszaæ myszkê w lewo i prawo wraz z wci¶niêtym lewym klawiszem.";
 
 Calendar._TT["PREV_YEAR"] = "Poprz. rok (przytrzymaj dla menu)";
-Calendar._TT["PREV_MONTH"] = "Poprz. miesiÄ…c (przytrzymaj dla menu)";
-Calendar._TT["GO_TODAY"] = "PokaÅ¼ dziÅ›";
-Calendar._TT["NEXT_MONTH"] = "Nast. miesiÄ…c (przytrzymaj dla menu)";
+Calendar._TT["PREV_MONTH"] = "Poprz. miesi±c (przytrzymaj dla menu)";
+Calendar._TT["GO_TODAY"] = "Poka¿ dzi¶";
+Calendar._TT["NEXT_MONTH"] = "Nast. miesi±c (przytrzymaj dla menu)";
 Calendar._TT["NEXT_YEAR"] = "Nast. rok (przytrzymaj dla menu)";
-Calendar._TT["SEL_DATE"] = "Wybierz datÄ™";
-Calendar._TT["DRAG_TO_MOVE"] = "PrzesuÅ„ okienko";
-Calendar._TT["PART_TODAY"] = " (dziÅ›)";
-Calendar._TT["MON_FIRST"] = "PokaÅ¼ PoniedziaÅ‚ek jako pierwszy";
-Calendar._TT["SUN_FIRST"] = "PokaÅ¼ NiedzielÄ™ jako pierwszÄ…";
+Calendar._TT["SEL_DATE"] = "Wybierz datê";
+Calendar._TT["DRAG_TO_MOVE"] = "Przesuñ okienko";
+Calendar._TT["PART_TODAY"] = " (dzi¶)";
+Calendar._TT["MON_FIRST"] = "Poka¿ Poniedzia³ek jako pierwszy";
+Calendar._TT["SUN_FIRST"] = "Poka¿ Niedzielê jako pierwsz±";
 Calendar._TT["CLOSE"] = "Zamknij";
-Calendar._TT["TODAY"] = "DziÅ›";
-Calendar._TT["TIME_PART"] = "(Shift-)klik | drag, aby zmieniÄ‡ wartoÅ›Ä‡";
+Calendar._TT["TODAY"] = "Dzi¶";
+Calendar._TT["TIME_PART"] = "(Shift-)klik | drag, aby zmieniæ warto¶æ";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y.%m.%d";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
 Calendar._TT["WK"] = "wk";
+
+// the following is to inform that "%s" is to be the first day of week
+// %s will be replaced with the day name.
+Calendar._TT["DAY_FIRST"] = "Prezentacja pierwszych %s dni";
+
+// This may be locale-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
+Calendar._TT["WEEKEND"] = "0,6";
