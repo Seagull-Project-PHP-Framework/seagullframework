760b3677fdc2ee7b7a1080d7d15301bbc82bbfb0
Demian Turner <demian@seagullsystems.com>
2006-07-14 01:33:47 +0000

    Admin screen done for FAQs

--- modules/faq/lang/english-iso-8859-15.php
+++ modules/faq/lang/english-iso-8859-15.php
@@ -12,6 +12,7 @@
         'Question' => 'Question',
         'Answer'   => 'Answer',
         'Date created' => 'Date created',
+        'Faq list' => 'Faq list',
 /*  list */
         'Questions' => 'Questions',
         'Answers' => 'Answers',
@@ -20,12 +21,14 @@
         'No FAQs found' => 'No FAQs found',
 /*  admin list */
         'new faq'  => 'new faq',
+        'edit faq' => 'edit faq',
         'reorder'  => 'reorder',
         'FAQ'      => 'FAQ',
         'Order'    => 'Order',
         'With selected faq(s)' => 'With selected faq(s)',
 /*  reorder */
         'reorder instruction' =>'Change the display order by selecting an item and clicking \'move up\' or \'move down\'. Be sure to click Finished when you\'re done.',
+        'reorder faqs' => 'reorder faqs',
 /*  messages */
         'faq updated successfully' => 'Your faq has been updated successfully',
         'faqs reordered successfully' => 'Your faqs have been reordered successfully',
