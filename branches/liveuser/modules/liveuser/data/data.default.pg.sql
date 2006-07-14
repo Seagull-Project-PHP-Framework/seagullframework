a03e947b914f1b00482059c82eeb5af012b309f3
aj <aj@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-07-14 08:53:13 +0000

    added PgSQL schema & data moved sample data into proper file renamed application from OPC to SEAGULL

---
+++ branches/liveuser/modules/liveuser/data/data.default.pg.sql
@@ -0,0 +1,37 @@
+BEGIN;
+
+INSERT INTO module VALUES ({SGL_NEXT_ID}, 0, 'messaging', 'Messaging', 'The ''Messaging'' module contains classes for sending internal Instant Messages, managing external email sending, and managing your contacts.', NULL, '48/module_messaging.png', '', NULL, NULL, NULL);
+
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'luusersmgr', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'luusersmgr_cmd_editgroups', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'luusersmgr_cmd_updategroups', '', (SELECT max(module_id) FROM module));
+
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_add', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_insert', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_edit', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_update', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_delete', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_list', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_editrights', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_updaterights', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_editmemebers', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lugroupsmgr_cmd_updatemembers', '', (SELECT max(module_id) FROM module));
+
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_add', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_insert', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_edit', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_update', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_delete', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_list', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_editperms', '', (SELECT max(module_id) FROM module));
+INSERT INTO permission VALUES ({SGL_NEXT_ID}, 'lurightsmgr_cmd_updateperms', '', (SELECT max(module_id) FROM module));
+
+-- Application
+INSERT INTO liveuser_applications VALUES ({SGL_NEXT_ID}, 'SEAGULL');
+
+-- Areas
+INSERT INTO liveuser_areas VALUES ({SGL_NEXT_ID}, 1, 'BACKEND');
+
+COMMIT;
---
+++ branches/liveuser/modules/liveuser/data/data.sample.my.sql
@@ -0,0 +1,122 @@
+-- 
+--  `liveuser_applications`
+-- 
+
+--INSERT INTO `liveuser_applications` VALUES (1, 'OPC');
+
+-- 
+--  `liveuser_area_admin_areas`
+-- 
+
+
+-- 
+--  `liveuser_areas`
+-- 
+
+--INSERT INTO `liveuser_areas` VALUES (1, 1, 'BACKEND');
+
+-- 
+--  `liveuser_group_subgroups`
+-- 
+
+
+-- 
+--  `liveuser_grouprights`
+-- 
+
+INSERT INTO `liveuser_grouprights` VALUES (38, 9, 1);
+INSERT INTO `liveuser_grouprights` VALUES (38, 6, 1);
+INSERT INTO `liveuser_grouprights` VALUES (38, 8, 1);
+INSERT INTO `liveuser_grouprights` VALUES (36, 9, 1);
+INSERT INTO `liveuser_grouprights` VALUES (36, 4, 1);
+INSERT INTO `liveuser_grouprights` VALUES (36, 6, 1);
+INSERT INTO `liveuser_grouprights` VALUES (36, 8, 1);
+
+-- 
+--  `liveuser_groups`
+-- 
+
+INSERT INTO `liveuser_groups` VALUES (38, 1, 'MODERATOR');
+INSERT INTO `liveuser_groups` VALUES (36, 1, 'GUEST');
+
+-- 
+--  `liveuser_groups_seq`
+-- 
+
+INSERT INTO `liveuser_groups_seq` VALUES (38);
+
+-- 
+--  `liveuser_groupusers`
+-- 
+
+INSERT INTO `liveuser_groupusers` VALUES (0, 38);
+
+-- 
+--  `liveuser_perm_users`
+-- 
+
+INSERT INTO `liveuser_perm_users` VALUES (1, '1', 1, '0');
+INSERT INTO `liveuser_perm_users` VALUES (2, '2', 1, '0');
+
+-- 
+--  `liveuser_right_implied`
+-- 
+
+
+-- 
+--  `liveuser_rights`
+-- 
+
+INSERT INTO `liveuser_rights` VALUES (30, 1, 'HUMAN_RIGHT', 'N', 'N', 'human right', '');
+
+-- 
+--  `liveuser_rights_seq`
+-- 
+
+INSERT INTO `liveuser_rights_seq` VALUES (30);
+
+-- 
+--  `liveuser_translations`
+-- 
+
+INSERT INTO `liveuser_translations` VALUES (5, 25, 4, 'en_iso_8859_15', 'dsf', 'sdf');
+INSERT INTO `liveuser_translations` VALUES (6, 26, 4, 'en_iso_8859_15', 'dsf', 'sdf');
+INSERT INTO `liveuser_translations` VALUES (12, 38, 3, 'en_iso_8859_15', 'moderator', 'User with permission to edit content');
+INSERT INTO `liveuser_translations` VALUES (9, 30, 4, 'en_iso_8859_15', 'Human right', 'All people have their rights and lefts');
+INSERT INTO `liveuser_translations` VALUES (10, 36, 3, 'en_iso_8859_15', 'guest', 'unauthorized users');
+
+-- 
+--  `liveuser_translations_seq`
+-- 
+
+INSERT INTO `liveuser_translations_seq` VALUES (12);
+
+-- 
+--  `liveuser_userrights`
+-- 
+
+
+-- 
+--  `liveuser_users`
+-- 
+
+
+-- 
+--  `right_permission`
+-- 
+
+INSERT INTO `right_permission` VALUES (7, 4, 52);
+INSERT INTO `right_permission` VALUES (8, 4, 55);
+INSERT INTO `right_permission` VALUES (15, 4, 3);
+INSERT INTO `right_permission` VALUES (14, 4, 4);
+INSERT INTO `right_permission` VALUES (13, 4, 1);
+INSERT INTO `right_permission` VALUES (12, 4, 2);
+INSERT INTO `right_permission` VALUES (16, 4, 7);
+INSERT INTO `right_permission` VALUES (17, 4, 6);
+INSERT INTO `right_permission` VALUES (26, 30, 55);
+INSERT INTO `right_permission` VALUES (25, 30, 52);
+INSERT INTO `right_permission` VALUES (24, 30, 104);
+INSERT INTO `right_permission` VALUES (31, 30, 5);
+INSERT INTO `right_permission` VALUES (32, 30, 51);
+INSERT INTO `right_permission` VALUES (33, 30, 86);
+INSERT INTO `right_permission` VALUES (34, 30, 90);
