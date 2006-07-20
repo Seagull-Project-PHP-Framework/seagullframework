b137ad77dfa0fea4da1dea03604cb7f48b67db24
Demian Turner <demian@seagullsystems.com>
2006-07-20 02:18:00 +0000

    event prep

--- www/ajaxServer.php
+++ www/ajaxServer.php
@@ -10,7 +10,7 @@ define('SGL_CACHE_LIBS', (is_file($varDir . '/ENABLE_LIBCACHE.txt'))
 
 require_once $rootDir .'/lib/SGL/FrontController.php';
 SGL_FrontController::init();
-require_once SGL_MOD_DIR . '/cms/classes/CmsDAO.php';
+require_once SGL_MOD_DIR . '/media/classes/MediaDAO.php';
 require_once 'HTML/AJAX/Server.php';
 
 class AutoServer extends HTML_AJAX_Server
@@ -18,9 +18,9 @@ class AutoServer extends HTML_AJAX_Server
 	// this flag must be set for your init methods to be used
 	var $initMethods = true;
 
-	function initCmsDAO()
+	function initMediaDAO()
 	{
-		$da = & CmsDAO::singleton();
+		$da = & MediaDAO::singleton();
 		$this->registerClass($da);
 	}
 }
