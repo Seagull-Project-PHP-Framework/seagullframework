d4080a74b39d82650004045005e208ca53fa4d4b
Demian Turner <demian@seagullsystems.com>
2006-07-20 02:19:44 +0000

    adding event module - GPL

---
+++ modules/media/classes/MediaDAO.php
@@ -0,0 +1,42 @@
+<?php
+class MediaDAO extends SGL_Manager
+{
+    /**
+     * @return CmsDAO
+     */
+    function MediaDAO()
+    {
+        parent::SGL_Manager();
+    }
+
+    function &singleton()
+    {
+        static $instance;
+
+        // If the instance is not there, create one
+        if (!isset($instance)) {
+            $instance = new MediaDAO();
+        }
+        return $instance;
+    }
+
+    function getMediaByFileType($id = null)
+    {
+        $constraint = (is_null($id)) ? '' : ' WHERE m.file_type_id = ' . $id;
+        $query = "
+            SELECT media_id,
+                m.name, file_size, mime_type,
+                m.date_created, description,
+                mt.name AS file_type_name,
+                u.username AS media_added_by
+            FROM
+                {$this->conf['table']['media']} m
+            JOIN file_type mt ON mt.file_type_id = m.file_type_id
+            LEFT JOIN usr u ON u.usr_id = m.added_by
+            $constraint
+            ORDER BY m.date_created DESC
+            ";
+        return $this->dbh->getAll($query);
+    }
+}
+?>
