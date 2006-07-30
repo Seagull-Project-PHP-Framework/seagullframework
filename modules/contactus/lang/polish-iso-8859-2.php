2de3f79f3bc3d3f5ef66935d597b5eeeb3dc3363
Demian Turner <demian@seagullsystems.com>
2006-07-30 13:38:04 +0000

    improvements to the Polish translation

--- modules/contactus/lang/polish-iso-8859-2.php
+++ modules/contactus/lang/polish-iso-8859-2.php
@@ -1,26 +1,32 @@
 <?php
+/*
+  Language-file polish-iso-8859-2.php
+  Author: Tomasz Osmialowski <ichtis@gmail.com>
+*/
     $words = array(
 /*  CONTACT US MGR  */
-        'Contact Us' => 'Contact Us',
-        'First Name' => 'First Name',
-        'Last Name' => 'Last Name',
+        'Contact Us' => 'Kontakt',
+        'First Name' => 'Imiê',
+        'Last Name' => 'Nazwisko',
         'Email' => 'Email',
-        'contact intro' => 'If you would like to get in touch please fill out the following form and submit your details.',
-        'Type of enquiry' => 'Type of enquiry',
-        'Comment' => 'Comment',
-        'Contact Enquiry from' => 'Contact Enquiry from',
+        'contact intro' => 'Je¿eli chcesz przes³aæ zapytanie proszê wype³nij poni¿szy formularz i prze¶lij swoje namiary.',
+        'Type of enquiry' => 'Rodzaj zapytania',
+        'Comment' => 'Tre¶æ',
+        'Contact Enquiry from' => 'Zapytanie z',
+        'Enter Captcha' => 'Proszê podaæ numer widoczny w polu poni¿ej po prawej',
         'aContactType' => array(
-            'Ogólna informacja' => 'Ogólna informacja',
+            'General enquiry' => 'Ogólne zapytanie',
             'Get a quote' => 'Pytanie',
             'Hosting info' => 'Propozycja do serwisu',
             'Site feedback' => 'Uwagi o serwisie',
             ),
 
 /*  ERROR MSGS  */
-        'You must enter your first name' => 'You must enter your first name',
-        'You must enter your last name' => 'You must enter your last name',
-        'You must fill in your comment' => 'You must fill in your comment',
-        'Your email is not correctly formatted' => 'Your email is not correctly formatted',
-        'You must enter your email' => 'You must enter your email',
+        'You must enter your first name' => 'Okre¶l imiê nadawcy',
+        'You must enter your last name' => 'Okre¶l nazwisko nadawcy',
+        'You must fill in your comment' => 'Wype³nij tre¶æ',
+        'Your email is not correctly formatted' => 'Format adresu email jest nieprawid³owy',
+        'You must enter your email' => 'Podaj adres email',
+        'You must enter the number to the left in this field' => 'Musisz podaæ ten sam numer jak w polu obok',
     );
 ?>
