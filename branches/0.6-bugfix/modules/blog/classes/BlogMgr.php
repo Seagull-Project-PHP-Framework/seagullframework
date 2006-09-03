3d84b322ee1261d726505e6d92e6b91060a00324
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-09-03 00:36:41 +0000

    Fixed infinite loop in s9y wrapper BlogMgr

--- branches/0.6-bugfix/modules/blog/classes/BlogMgr.php
+++ branches/0.6-bugfix/modules/blog/classes/BlogMgr.php
@@ -79,7 +79,7 @@ class BlogMgr extends SGL_Manager
 
         require_once 'HTTP/Request.php';
         $phpSelfParts = split('\?', $_SERVER['PHP_SELF']);
-        $s9yUri = SGL_BASE_URL . '/serendipity/?'.@$phpSelfParts[1];
+        $s9yUri = SGL_BASE_URL . '/serendipity/index.php?'.@$phpSelfParts[1];
         $req = &new HTTP_Request($s9yUri);
         $req->sendRequest();
         $output->blogData = $req->getResponseBody();
