d2880aa4102f7a9c73745af256862f9db7a8cf8a
Demian Turner <demian@seagullsystems.com>
2006-09-07 02:01:12 +0000

    small comment fixes

--- modules/comment/classes/CommentDAO.php
+++ modules/comment/classes/CommentDAO.php
@@ -1,4 +1,51 @@
 <?php
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | Copyright (c) 2006, Demian Turner                                         |
+// | All rights reserved.                                                      |
+// |                                                                           |
+// | Redistribution and use in source and binary forms, with or without        |
+// | modification, are permitted provided that the following conditions        |
+// | are met:                                                                  |
+// |                                                                           |
+// | o Redistributions of source code must retain the above copyright          |
+// |   notice, this list of conditions and the following disclaimer.           |
+// | o Redistributions in binary form must reproduce the above copyright       |
+// |   notice, this list of conditions and the following disclaimer in the     |
+// |   documentation and/or other materials provided with the distribution.    |
+// | o The names of the authors may not be used to endorse or promote          |
+// |   products derived from this software without specific prior written      |
+// |   permission.                                                             |
+// |                                                                           |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       |
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT         |
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR     |
+// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      |
+// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,     |
+// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT          |
+// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     |
+// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY     |
+// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT       |
+// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE     |
+// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.      |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+// | Seagull 0.6                                                               |
+// +---------------------------------------------------------------------------+
+// | CommentDAO.php                                                            |
+// +---------------------------------------------------------------------------+
+// | Authors:   Demian Turner <demian@phpkitchen.com>                          |
+// +---------------------------------------------------------------------------+
+// $Id: CommentDAO.php,v 1.14 2005/06/21 23:26:24 demian Exp $
+
+/**
+ * Data access methods for the publisher module.
+ *
+ * @package seagull
+ * @subpackage comment
+ * @author  Demian Turner <demian@phpkitchen.com>
+ * @copyright Demian Turner 2006
+ */
 class CommentDAO extends SGL_Manager
 {
     /**
@@ -22,7 +69,7 @@ class CommentDAO extends SGL_Manager
 
     /**
      * For retrieving comments associated with entites,
-     * ie articles = 'article', faqs = 'faq'
+     * ie articles = 'articleview', faqs = 'faq'
      *
      * @param string $entity
      * @param integer $id
