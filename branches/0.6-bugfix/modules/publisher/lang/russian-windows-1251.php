e5588693fe0e7a013a11e37d70b428414515b2ba
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-10-24 11:46:20 +0000

    russian updates, fixes #1266

--- branches/0.6-bugfix/modules/publisher/lang/russian-windows-1251.php
+++ branches/0.6-bugfix/modules/publisher/lang/russian-windows-1251.php
@@ -1,151 +1,175 @@
 <?php
+
 $words = array(
 
-/* Article MGR */
-    
+/* ArticleMgr */
+
     // titles
     'Article Manager'         => 'Управление статьями',
-    'Article Manager :: Add'  => 'Управление статьями :: добавление',
-    'Article Manager :: Edit' => 'Управление статьями :: редактирование',
+    'Article Manager :: Add'  => 'Управление статьями :: Добавление',
+    'Article Manager :: Edit' => 'Управление статьями :: Редактирование',
     'Article Browser'         => 'Просмотр статей',
-    
+
     // list
-    'New Article'             => 'Добавить статью',
-    'Article filter'          => 'Фильтр',
-    'Article type'            => 'Тип статьи',
-    'Article list'            => 'Список статей',
-    'Article Name'            => 'Название',
-    'Date created'            => 'Дата создания',
-    'Start Date'              => 'Дата начала',
-    'Expiry Date'             => 'Дата окончания',
-    'Modif. By'               => 'Редактировано',
-    'approve'                 => 'утвердить',
-    'publish'                 => 'опубликовать',
-    'archive'                 => 'архивировать',
-    'For Approval'            => 'для утверждения',
-    'Being Edited'            => 'редактируется',
-    'Approved'                => 'утверждено',
-    'Published'               => 'опубликовано',
-    'Archived'                => 'архивировано',
-    'Deleted'                 => 'удалено',
-    'article'                 => 'статья',
-    
+    'New Article'    => 'Добавить статью',
+    'Article filter' => 'Фильтр статей',
+    'Article type'   => 'Тип статьи',
+    'Article list'   => 'Список статей',
+    'Article Name'   => 'Название статьи',
+    'Date created'   => 'Дата создания',
+    'Start Date'     => 'Дата начала',
+    'Expiry Date'    => 'Дата окончания',
+    'Modif. By'      => 'Редактировано',
+    'article'        => 'статья',
+
+    // flow
+    'approve'      => 'утвердить',       // actions
+    'publish'      => 'опубликовать',
+    'archive'      => 'архивировать',
+    'For Approval' => 'Для утверждения', // statuses
+    'Being Edited' => 'Редактируется',
+    'Approved'     => 'Утверждено',
+    'Published'    => 'Опубликовано',
+    'Archived'     => 'Архивировано',
+    'Deleted'      => 'Удалено',
+
     // add / edit
-    'Content'                 => 'Содержание',
-    'Editing options'         => 'Настройки',
-    'Author'                  => 'Автор',    
-    'No other text entered'   => 'Текст не введен',
-    'Flesch score'            => 'Flesch Score', // ???
-    'No expire'               => 'Не устаревает',
-        
+    'Content'               => 'Содержание',
+    'Editing options'       => 'Настройки',
+    'Author'                => 'Автор',
+    'No other text entered' => 'Текст не введен',
+    'Flesch score'          => 'Flesch Score', // ???
+    'No expire'             => 'Не устаревает',
+
     // messages
     'Article successfully added'                             => 'Статья успешно добавлена',
     'Article successfully updated'                           => 'Статья успешно отредактирована',
     'Article status has been successfully changed'           => 'Статус статьи успешно сменен',
     'The selected article(s) have successfully been deleted' => 'Выбранные статьи успешно удалены',
 
-    
-/* ArticleView MGR */
-    
+
+/* ArticleViewMgr */
+
     // view / summary
-    'Current Category'        => 'Текущая категория',
-    'No lead article found'   => 'Основная статья не найдена',
-    'No articles found'       => 'Статей не найдено',
-    'No documents found'      => 'Документов не найдено',
-    'Related Articles'        => 'См. также (статьи)',
-    'Related Documents'       => 'См. также (документы)',
-    'contributed by'          => 'Автор', // perevod ne po soderzhaniju, a po smyslu
-    'full story'              => 'Полный текст', // perevod ne po soderzhaniju, a po smyslu
-    
-    
-/* Document MGR */
+    'Current Category'      => 'Текущая категория',
+    'No lead article found' => 'Основная статья не найдена',
+    'No articles found'     => 'Статей не найдено',
+    'No documents found'    => 'Документов не найдено',
+    'Related Articles'      => 'Схожие статьи',
+    'Related Documents'     => 'Прикрепленные документы',
+    'contributed by'        => 'автор', // написал / создал
+    'full story'            => 'Продолжение', // ориг. - Continue reading
+
+
+/* DocumentMgr */
 
     // titles
-    'Document Manager'        => 'Управление файлами',
-    
+    'Document Manager' => 'Управление файлами',
+
+    // actions
+    'New Asset'     => 'Новый файл',
+    'Editing Asset' => 'Редактирование файла',
+
     // list
-    'New Asset'               => 'Добавить файл',
-    'Document filter'         => 'Фильтр',
-    'select a category'       => 'Выберите категорию',
-    'Document list'           => 'Список файлов',
-    'showing results for'     => 'Показывать', // perevod ne po soderzhaniju, a po smyslu
-    'Asset Name'              => 'Название',
-    'Size'                    => 'Размер',
-    'Type'                    => 'Тип',
-    'Date Added'              => 'Добавлено',
-    'Owner'                   => 'Добавил',
-    'Download'                => 'Скачать',
-    'Document type'           => 'Тип документа',
-    'asset'                   => 'файл',
-    'choose'                  => 'выбрать',
-    'whole DB'                => 'все категории',
-    'this category'           => 'текущая категория',
-    'preview'                 => 'просмотр',
-    
+    'Document filter'     => 'Фильтр документов',
+    'select a category'   => 'выберите категорию',
+    'Document list'       => 'Список файлов',
+    'showing results for' => 'показывать', //
+    'Asset Name'          => 'Название файла',
+    'Size'                => 'Размер',
+    'Type'                => 'Тип',
+    'Date Added'          => 'Добавлено',
+    'Owner'               => 'Владелец',
+    'Download'            => 'Скачать',
+    'Document type'       => 'Тип документа',
+    'asset'               => 'файл',
+    'choose'              => 'выбрать',
+    'whole DB'            => 'все категории',
+    'this category'       => 'текущая категория',
+    'close'               => 'закрыть',
+    'preview'             => 'просмотр', // предварительный
+
     // add / edit
-    'Editing Asset'           => 'Редактирование файла',
-    'Locate'                  => 'Файл', // perevod ne po soderzhaniju, a po smyslu
-    'Mime Type'               => 'Mime тип',
-    'Upload'                  => 'Загрузить',
-    'Category'                => 'Категория',
-    'Change Category'         => 'Изменить категорию',
-    'Description'             => 'Описание',
-    
-    // messages
-    'The asset has successfully been added'       => 'Файл успешно добавлен',
-    'The asset has successfully been deleted'     => 'Выбранные файлы успешно удалены',
-    'The asset has successfully been updated'     => 'Файл успешно отредактирован',
-    
+    'Locate'          => 'Указать',
+    'Mime Type'       => 'Mime тип',
+    'Upload'          => 'Загрузить',
+    'Category'        => 'Категория',
+    'Change Category' => 'Изменить категорию',
+    'Description'     => 'Описание',
+
     // validate
     'You must select a file to upload'                   => 'Пожалуйста, укажите файл для загрузки',
     'Error: Not a recognised file type'                  => 'Ошибка: неизвестный тип файла',
     'Error: A file with this name already exists'        => 'Ошибка: файл с таким именем существует',
-    'There was an error attempting to download the file' => 'Ошибка при загрезке файла',
+    'There was an error attempting to download the file' => 'Ошибка загрузки файла',
     'The specified file does not appear to exist'        => 'Указанный файл не существует',
-    
-    // form alerts
-    'Please wait until upload is finished' => 'Пожалуйста, подождите пока файл не загрузится',
-    'Please select a file to upload'       => 'Пожалуйста, укажите файл для загрузки',
-    'Please wait while document uploads'   => 'Пожалуйста, подождите... файл загружается',
-    
 
-/* WikiScrape MGR */
-    
+    // form alerts
+    'Please wait until upload is finished' => 'Пожалуйста, подождите пока загрузка не закончится',
+    'Please select a file to upload'       => 'Пожалуйста, укажите файл для загрузки',
+    'Please wait while document uploads'   => 'Пожалуйста, подождите пока документ загружается',
+
+    // messages
+    'The asset has successfully been added'   => 'Файл успешно добавлен',
+    'The asset has successfully been deleted' => 'Файлы успешно удалены',
+    'The asset has successfully been updated' => 'Файл успешно отредактирован',
+
+
+/* FileMgr */
+
+    // messages
+    'You do not have read permissions for this file' => 'У Вас нет прав на чтение данного файла',
+
+
+/* DocumentViewMgr */
+
+    // titles
+    'Document Browser' => 'Просмотр документов',
+
+    // list
+    'view'     => 'просмотреть',
+    'download' => 'скачать',
+
+
+/* WikiScrapeMgr */
+
     // titles
     'WikiScrape Manager' => 'Управление WikiScrape',
-    
 
-/* ContentType MGR */
 
-    //
-    // TODO: filter following messages (delete obsolete), 
-    //       cheeck ContentType templates, when they will be ready
-    //
+/* ContentTypeMgr */
 
     // titles
-    'Content Type Manager' => 'Content Type Manager',
+    'Content Type Manager' => 'Управление типами содержания',
 
     // types
     'Title'    => 'Заголовок',
     'BodyHtml' => 'HTML текст',
     'NewsHtml' => 'Текст',
-    
-    'select a type to create a new article' => 'укажите тип новой статьи',
+
+    // actions
+    'Add New Content Type' => 'Добавление нового типа содержания',
+    'Editing Content Type' => 'Редактирование типа содержания',
+
+    //
+    // отсортировать и перевести; на данный момент здесь слишком много устаревших
+    // ключевых слов, как и в английском варианте
+    //
+
+    'select a type to create a new article' => 'укажите тип для создания новой статьи',
     'Current View Wysiwyg' => 'Current View Htmlers', // ?
     'View Wysiwyg' => 'View Wysiwyg', // ?
     'Current View Html' => 'Current View Html', // ?
     'View Html' => 'View Html', // ?
     'Select Font' => 'Выбрать Шрифт',
-    
-    'Content Type Manager' => 'Content Type Manager',
+
     'Number of fields' => 'Number of fields',
     'field' => 'field',
     'New content type' => 'New content type',
     'Give a name to this field and select its type' => 'Give a name to this field and select its type',
-    'Add Type' => 'Add Type',
     'With selected content type(s)' => 'With selected content type(s)',
     'content type has successfully been added' => 'content type has successfully been added',
-    'content type has successfully been deleted' => 'content type has successfully been deleted',        
+    'content type(s) has successfully been deleted' => 'content type(s) has successfully been deleted',
     'content type has successfully been updated' => 'content type has successfully been updated',
     'Through the Publisher module Seagull allows you to create three types of content.This is easily customisable however only 3 types will be discussed here:'=>'Through the Publisher module Seagull allows you to create three types of content.This is easily customisable however only 3 types will be discussed here:',
     'What you see when you click the Articles tab in the front end is a document collection. Creating articles of type Html Article allows you to place your content in a hierarchy that you build using the Categories button above.  This can be useful for intranet applications, or if you have a large body of work that needs to be categorised. Document collection articles will be displayed with all articles from the same category appearing in the Related Articles box  on the right. Similarly, all files uploaded to the same category with the Document Manager will appear in the Related Documents box.'=>'What you see when you click the \'Articles\' tab in the front end is a document collection. Creating articles of type \'Html Article\' allows you to place your content in a hierarchy that you build using the \'Categories\' button above. This can be useful for intranet applications, or if you have a large body of work that needs to be categorised. Document collection articles will be displayed with all articles from the same category appearing in the \'Related Articles\' box on the right. Similarly, all files uploaded to the same category with the Document Manager will appear in the \'Related Documents\' box.',
@@ -156,4 +180,5 @@ $words = array(
     'Contributed by' =>'Contributed by',
     'Please select an article type' => 'Please select an article type',
 );
+
 ?>
