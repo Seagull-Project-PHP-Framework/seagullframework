0e664edb620d3c01e745fd80e6fa247461e91dd3
Demian Turner <demian@seagullsystems.com>
2007-03-14 12:08:28 +0000

    better integration of moduleDirOverride

--- lib/SGL/Task/Init.php
+++ lib/SGL/Task/Init.php
@@ -326,6 +326,31 @@ class SGL_Task_SetupConstantsFinish extends SGL_Task
 {
     function run($conf)
     {
+        // On install, $conf is empty let's load it
+        if (empty($conf) && file_exists(SGL_ETC_DIR . '/customInstallDefaults.ini')) {
+            $c = &SGL_Config::singleton();
+            $conf1 = $c->load(SGL_ETC_DIR . '/customInstallDefaults.ini');
+            if (isset($conf1['path']['moduleDirOverride'])) {
+                $conf['path']['moduleDirOverride'] = $conf1['path']['moduleDirOverride'];
+            }
+        // On re-install or INSTALL_COMPLETE
+        } elseif (count($conf)) {
+            //  set constant to represent profiling mode so it can be used in Controller
+            define('SGL_PROFILING_ENABLED', ($conf['debug']['profiling']) ? true : false);
+            define('SGL_SEAGULL_VERSION', $conf['tuples']['version']);
+
+            //  which degree of error severity before emailing admin
+            define('SGL_EMAIL_ADMIN_THRESHOLD',
+                SGL_String::pseudoConstantToInt($conf['debug']['emailAdminThreshold']));
+            define('SGL_BASE_URL', $conf['site']['baseUrl']);
+
+            //  add additional search paths
+            if (!empty($conf['path']['additionalIncludePath'])) {
+                $ok = ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR
+                    . $conf['path']['additionalIncludePath']);
+            }
+        }
+
         if (isset($conf['path']['webRoot'])) {
             define('SGL_WEB_ROOT', $conf['path']['webRoot']);
         } elseif (defined('SGL_PEAR_INSTALLED')) {
@@ -360,24 +385,6 @@ class SGL_Task_SetupConstantsFinish extends SGL_Task
             define('PEAR_LOG_NOTICE',   5);     /** Normal but significant */
             define('PEAR_LOG_INFO',     6);     /** Informational */
             define('PEAR_LOG_DEBUG',    7);     /** Debug-level messages */
-        }
-
-        if (count($conf)) {
-
-            //  set constant to represent profiling mode so it can be used in Controller
-            define('SGL_PROFILING_ENABLED', ($conf['debug']['profiling']) ? true : false);
-            define('SGL_SEAGULL_VERSION', $conf['tuples']['version']);
-
-            //  which degree of error severity before emailing admin
-            define('SGL_EMAIL_ADMIN_THRESHOLD',
-                SGL_String::pseudoConstantToInt($conf['debug']['emailAdminThreshold']));
-            define('SGL_BASE_URL', $conf['site']['baseUrl']);
-
-            //  add additional search paths
-            if (!empty($conf['path']['additionalIncludePath'])) {
-                $ok = ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR
-                    . $conf['path']['additionalIncludePath']);
-            }
         }
     }
 }
