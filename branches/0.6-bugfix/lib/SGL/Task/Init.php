a8a0310d571dbca6af651bd21e49c6b1e9e8aec4
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-05-12 16:07:39 +0000

    made installation paths more flexible

--- branches/0.6-bugfix/lib/SGL/Task/Init.php
+++ branches/0.6-bugfix/lib/SGL/Task/Init.php
@@ -76,7 +76,20 @@ class SGL_Task_SetupPaths extends SGL_Task
                     define('SGL_PATH', $conf['path']['installRoot']);
                 }
             } else {
-                define('SGL_PATH', dirname(dirname(dirname(dirname(__FILE__)))));
+                //  accept custom path from installer
+//                if (!empty($_POST['customRootPath'])) {
+//                    $suppliedPath = $_SESSION['customRootPath'] = $_POST['customRootPath'];
+//                } else {
+//                    $suppliedPath =  isset($_SESSION['customRootPath'])
+//                        ? $_SESSION['customRootPath']
+//                        : '';
+//                }
+//                $sglPath = (!empty($suppliedPath))
+//                    ? $_POST['customRootPath']
+//                    :  dirname(dirname(dirname(dirname(__FILE__))));
+//                $rootDir;
+//                define('SGL_PATH', $sglPath);
+                define('SGL_PATH', $GLOBALS['rootDir']);
             }
             define('SGL_LIB_PEAR_DIR', SGL_PATH . '/lib/pear');
         }
