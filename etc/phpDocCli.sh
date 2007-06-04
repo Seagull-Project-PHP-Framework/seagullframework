18125512e925b2dc90413fba2dcb27d891aa3498
Demian Turner <demian@seagullsystems.com>
2007-06-04 20:35:11 +0000

    bugfix to trunk merge

--- etc/phpDocCli.sh
+++ etc/phpDocCli.sh
@@ -56,7 +56,7 @@ FILES=/var/www/html/seagull/branches/0.6-bugfix/lib/SGL.php
 #  *
 #  * @var               string PATH_PHPDOC
 #  */
-PATH_PHPDOC=/usr/local/bin/phpdoc
+PATH_PHPDOC=/usr/bin/phpdoc
 
 #/**
 #  * where documentation will be put
