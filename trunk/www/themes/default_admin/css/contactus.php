7e981d8f2291bca32fc999beacc58c21c72e35b4
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-07-02 15:51:52 +0000

    adding admin screens to contactus module

---
+++ trunk/www/themes/default_admin/css/contactus.php
@@ -0,0 +1,19 @@
+
+/*
+=======================ContactUsAdmin Manager==========================*/
+/*
+-- contactusList.html --------------------------------------------*/
+
+/*
+-- contactusView.html --------------------------------------------*/
+#frmItem p label {
+    width: 150px;
+}
+#frmItem p.comment {
+    line-height: 1.2em;
+}
+#frmItem p.comment span {
+    display: block;
+    margin-left: 170px;
+}
+
