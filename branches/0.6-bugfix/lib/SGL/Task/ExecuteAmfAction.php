41ddf89973495f124b2d1bcf2629bfd8f62f5276
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-08-06 22:59:02 +0000

    removing conf class property -3

--- branches/0.6-bugfix/lib/SGL/Task/ExecuteAmfAction.php
+++ branches/0.6-bugfix/lib/SGL/Task/ExecuteAmfAction.php
@@ -18,48 +18,46 @@ class SGL_Task_ExecuteAmfAction extends SGL_ProcessRequest
         $moduleName = $req->getModuleName();
         $method = $req->getActionName();
 
-        
+
 		$gateway = new Gateway();
-		
+
 		//Set where the services classes are loaded from, *with trailing slash*
         $gateway->setClassPath(SGL_MOD_DIR . '/' .($moduleName) . '/classes/');
 
-		
+
 		//Set where class mappings are loaded from (ie: for VOs)
 		$gateway->setClassMappingsPath(SGL_MOD_DIR . '/' .($moduleName) . '/classes/vo/');
 
 		//$gateway->setCharsetHandler("utf8_decode", "ISO-8859-1", "ISO-8859-1");
-		
+
 		//Error types that will be rooted to the NetConnection debugger
 		$gateway->setErrorHandling(E_ALL ^ E_NOTICE);
-		
-		if(PRODUCTION_SERVER)
+
+		if (PRODUCTION_SERVER)
 		{
 			//Disable profiling, remote tracing, and service browser
 			$gateway->disableDebug();
 		}
-		
-		//If you are running into low-level issues with corrupt messages and 
+
+		//If you are running into low-level issues with corrupt messages and
 		//the like, you can add $gateway->logIncomingMessages('path/to/incoming/messages/');
 		//and $gateway->logOutgoingMessages('path/to/outgoing/messages/'); here
 		//$gateway->logIncomingMessages('in/');
 		//$gateway->logOutgoingMessages('out/');
-		
+
 		//Explicitly disable the native extension if it is installed
 		//$gateway->disableNativeExtension();
-	
-		//Enable gzip compression of output if zlib is available, 
+
+		//Enable gzip compression of output if zlib is available,
 		//beyond a certain byte size threshold
 		$gateway->enableGzipCompression(25*1024);
-		
+
 		//Service now
 		ob_start();
 		$gateway->service();
 		$output->data = ob_get_contents();
 		ob_end_clean();
-
     }
-
 }
 
 ?>
