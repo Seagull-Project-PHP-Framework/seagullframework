22b5cf37ff57e9fe7b2c695b5b09034944bef519
Demian Turner <demian@seagullsystems.com>
2007-08-06 22:51:02 +0000

    removing conf class property -2

--- lib/SGL/Task/XmlToPhpUnserializer.php
+++ lib/SGL/Task/XmlToPhpUnserializer.php
@@ -18,7 +18,7 @@ class SGL_Task_XmlToPhpUnserializer extends SGL_DecorateProcess
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             $req = $input->getRequest();
-            $entityName = $this->conf['REST']['entityName'];
+            $entityName = SGL_Config::get('REST.entityName');
             $xml = $req->get($entityName, $allowTags = true);
         	$unserializer = &new XML_Unserializer();
         	$unserializer->setOption('tagAsClass', true);
@@ -42,7 +42,7 @@ class SGL_Task_XmlToPhpUnserializer extends SGL_DecorateProcess
      */
     function getKeysToIgnore()
     {
-        $keys = $this->conf['REST']['keysToIgnore'];
+        $keys = SGL_Config::get('REST.keysToIgnore');
         $aKeys = explode(',', $keys);
         return $aKeys;
     }
@@ -56,7 +56,7 @@ class SGL_Task_XmlToPhpUnserializer extends SGL_DecorateProcess
      */
     function getComplexTypes()
     {
-        $keys = $this->conf['REST']['complexType'];
+        $keys = SGL_Config::get('REST.complexType');
         $aKeys = explode(',', $keys);
         $aRet = array();
         foreach ($aKeys as $key) {
