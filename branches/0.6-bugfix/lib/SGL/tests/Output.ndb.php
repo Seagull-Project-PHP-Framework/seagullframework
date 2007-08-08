eeb1fbb72ba0ae4661036b4afd8290eedaeeb7b7
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-08-08 14:41:51 +0000

    translation more flexible

--- branches/0.6-bugfix/lib/SGL/tests/Output.ndb.php
+++ branches/0.6-bugfix/lib/SGL/tests/Output.ndb.php
@@ -20,9 +20,9 @@ class TestOutput extends UnitTestCase
     {
         $this->aTrans = $GLOBALS['_SGL']['TRANSLATION'];
         $GLOBALS['_SGL']['TRANSLATION'] = array(
-            'my string with %param1 one param' => 'my string with %param1 one param',
-            'my string with %param1 one param and second %param2 param' => 'my string with %param1 one param and second %param2 param',
-            'my string with %1 one param and second %2 param' => 'my string with %1 one param and second %2 param'
+            'my string with %param1% one param' => 'my string with %param1% one param',
+            'my string with %param1% one param and second %param2% param' => 'my string with %param1% one param and second %param2% param',
+            'my string with %1% one param and second %2% param' => 'my string with %1% one param and second %2% param'
         );
     }
 
@@ -38,7 +38,7 @@ class TestOutput extends UnitTestCase
         $output->value1 = 'my value 1';
 
         // translate string with one argument
-        $ret = $output->translate('my string with %param1 one param', 'vprintf',
+        $ret = $output->translate('my string with %param1% one param', 'vprintf',
             'param1|value1');
         $expected = 'my string with ' . $output->value1 . ' one param';
         $this->assertEqual($ret, $expected);
@@ -47,13 +47,13 @@ class TestOutput extends UnitTestCase
         $output->value2 = 'my value 2';
 
         // translate string with two arguments
-        $ret = $output->translate('my string with %param1 one param and second %param2 param',
+        $ret = $output->translate('my string with %param1% one param and second %param2% param',
             'vprintf', 'param1|value1||param2|value2');
         $expected = 'my string with ' . $output->value1
             . ' one param and second ' . $output->value2 . ' param';
         $this->assertEqual($ret, $expected);
 
-        $ret = $output->translate('my string with %1 one param and second %2 param',
+        $ret = $output->translate('my string with %1% one param and second %2% param',
             'vprintf', 'value1||value2');
         $expected = 'my string with ' . $output->value1
             . ' one param and second ' . $output->value2 . ' param';
