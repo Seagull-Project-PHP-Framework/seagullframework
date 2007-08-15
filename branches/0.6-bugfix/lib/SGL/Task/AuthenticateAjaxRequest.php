0fad1d734ca668ac26b5668285e07650214bef7d
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-08-15 13:50:16 +0000

    fixing ajax error handling -1

--- branches/0.6-bugfix/lib/SGL/Task/AuthenticateAjaxRequest.php
+++ branches/0.6-bugfix/lib/SGL/Task/AuthenticateAjaxRequest.php
@@ -38,8 +38,7 @@ class SGL_Task_AuthenticateAjaxRequest extends SGL_Task_AuthenticateRequest
             $aMethods = array_map('trim', $aMethods);
             if (in_array($actionName, $aMethods)) {
                 //  check that session is valid
-                if (true) {
-                //if (!$session->isValid()) {
+                if (!$session->isValid()) {
                     SGL::raiseError('authentication required');
                 //  or timed out
                 } elseif ($timeout) {
