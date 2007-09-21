9ecf5cf62c9ac0be58b13900c181ad39bf6d3158
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-09-21 11:04:21 +0000

    removed tix

--- branches/0.6-bugfix/lib/SGL/Output.php
+++ branches/0.6-bugfix/lib/SGL/Output.php
@@ -85,11 +85,11 @@ class SGL_Output
                         if (isset($output) && is_a($output, 'SGL_Output')) {
                             $aResultParams[$aStringValues[0]] = isset($output->{$aStringValues[1]})
                                 ? $output->{$aStringValues[1]}
-                                : $aStringValues[1];
+                                : null;
                         } elseif (is_a($this, 'SGL_Output')) {
                             $aResultParams[$aStringValues[0]] = isset($this->{$aStringValues[1]})
                                 ? $this->{$aStringValues[1]}
-                                : $aStringValues[1];
+                                : null;
                         }
                     }
                 } else {
