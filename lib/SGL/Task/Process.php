625c1008af713e22abe319cc56df8138f01b1a10
Dmitri Lakachauskis <lakiboy83@gmail.com>
2008-01-31 13:28:00 +0000

    factored out output vars setup in separate method in SGL_Task_BuildOutputData

--- lib/SGL/Task/Process.php
+++ lib/SGL/Task/Process.php
@@ -876,49 +876,81 @@ class SGL_Task_DiscoverClientOs extends SGL_DecorateProcess
  *
  * @package Task
  * @author  Demian Turner <demian@phpkitchen.com>
+ * @author  Dmitri Lakachauskis <lakiboy83@gmail.com>
  */
 class SGL_Task_BuildOutputData extends SGL_DecorateProcess
 {
+    /**
+     * Main routine.
+     *
+     * @access public
+     *
+     * @param SGL_Request $input
+     * @param SGL_Output $output
+     */
     function process(&$input, &$output)
     {
         $this->processRequest->process($input, $output);
 
         SGL::logMessage(null, PEAR_LOG_DEBUG);
+        SGL_Task_BuildOutputData::addOutputData($output);
+    }
 
-        //  setup login stats
+    /**
+     * Adds output vars to SGL_Output object.
+     *
+     * @access public
+     *
+     * @param SGL_Output $output
+     */
+    function addOutputData(&$output)
+    {
+        // setup login stats
         if (SGL_Session::getRoleId() > SGL_GUEST) {
-            $output->loggedOnUser = $_SESSION['username'];
+            $output->loggedOnUser   = SGL_Session::getUsername();
             $output->loggedOnUserID = SGL_Session::getUid();
-            $output->loggedOnSince = strftime("%H:%M:%S", $_SESSION['startTime']);
-            $output->loggedOnDate = strftime("%B %d", $_SESSION['startTime']);
-            $output->remoteIp = $_SERVER['REMOTE_ADDR'];
-            $output->isMember = true;
+            $output->loggedOnSince  = strftime("%H:%M:%S", SGL_Session::get('startTime'));
+            $output->loggedOnDate   = strftime("%B %d", SGL_Session::get('startTime'));
+            $output->isMember       = true;
         }
-        $output->currUrl          = $_SERVER['PHP_SELF'];
-        $output->currLang         = SGL::getCurrentLang();
-        $output->currFullLang     = $_SESSION['aPrefs']['language'];
-        $output->langDir          = ($output->currLang == 'ar' || $output->currLang == 'he')
+
+        // request data
+        $output->remoteIp = $_SERVER['REMOTE_ADDR'];
+        $output->currUrl  = $_SERVER['PHP_SELF'];
+
+        // lang data
+        $output->currLang     = SGL::getCurrentLang();
+        $output->charset      = SGL::getCurrentCharset();
+        $output->currFullLang = $_SESSION['aPrefs']['language'];
+        $output->langDir      = ($output->currLang == 'ar'
+                || $output->currLang == 'he')
             ? 'rtl' : 'ltr';
-        $output->theme            = isset($_SESSION['aPrefs']['theme'])
+
+        // setup theme
+        $output->theme = isset($_SESSION['aPrefs']['theme'])
             ? $_SESSION['aPrefs']['theme']
             : 'default';
         // check if theme is affected by the current manager
-        $mgrName = SGL_Inflector::caseFix(get_class($output->manager));
-        if (SGL_Config::get("$mgrName.theme")) {
-            $output->theme = SGL_Config::get("$mgrName.theme");
+        if (isset($output->manager)) {
+            $mgrName = SGL_Inflector::caseFix(get_class($output->manager));
+            if (SGL_Config::get("$mgrName.theme")) {
+                $output->theme = SGL_Config::get("$mgrName.theme");
+            }
         }
-        $output->charset          = $GLOBALS['_SGL']['CHARSET'];
+
+        // Setup SGL data
+        $c = &SGL_Config::singleton();
+        $output->conf             = $c->getAll();
         $output->webRoot          = SGL_BASE_URL;
         $output->imagesDir        = SGL_BASE_URL . '/themes/' . $output->theme . '/images';
         $output->versionAPI       = SGL_SEAGULL_VERSION;
         $output->sessID           = SGL_Session::getId();
-        $output->scriptOpen       = "\n<script type='text/javascript'>\n//<![CDATA[\n";
-        $output->scriptClose      = "\n//]]>\n</script>\n";
         $output->isMinimalInstall = SGL::isMinimalInstall();
-        $c = &SGL_Config::singleton();
-        $output->conf             = $c->getAll();
-        $output->showExecutionTimes = ($_SESSION['aPrefs']['showExecutionTimes'] == 1 )
-            ? true : false;
+
+        // Additional information
+        $output->scriptOpen         = "\n<script type='text/javascript'>\n//<![CDATA[\n";
+        $output->scriptClose        = "\n//]]>\n</script>\n";
+        $output->showExecutionTimes = $_SESSION['aPrefs']['showExecutionTimes'];
     }
 }
 
