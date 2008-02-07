3687796e4ce84e89fa9a5a08e53520e46f9b648c
Dmitri Lakachauskis <lakiboy83@gmail.com>
2008-02-07 10:27:49 +0000

    fix for ->getActionName() for AMF

--- lib/SGL/Request/Amf.php
+++ lib/SGL/Request/Amf.php
@@ -10,5 +10,16 @@ class SGL_Request_Amf extends SGL_Request_Ajax
         $this->type = SGL_REQUEST_AMF;
         return true;
     }
+
+    function getActionName()
+    {
+        $container = ucfirst($this->getModuleName()) . 'AmfProvider';
+        $ok = preg_match(
+            "/$container\.([a-zA-Z_0-9]+)/",
+            $GLOBALS['HTTP_RAW_POST_DATA'],
+            $aMatches
+        );
+        return $ok ? $aMatches[1] : 'default';
+    }
 }
 ?>
