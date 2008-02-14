ad8a756e1565158dd38e72f9347f65ad0386ccc5
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-02-14 16:24:33 +0000

    creating 0.9

---
+++ branches/0.9-bugfix/etc/get_seagull_blocks.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Returns systime in ms.
+ *
+ * @return string   Execution time in milliseconds
+ */
+function getSystemTime()
+{
+    $time = gettimeofday();
+    $resultTime = $time['sec'] * 1000;
+    $resultTime += floor($time['usec'] / 1000);
+    return $resultTime;
+}
+//  start timer
+define('SGL_START_TIME', getSystemTime());
+$rootDir = dirname(__FILE__) . '/..';
+$varDir = dirname(__FILE__) . '/../var';
+if (is_file($rootDir .'/lib/SGL/FrontController.php')) {
+    require_once $rootDir .'/lib/SGL/FrontController.php';
+}
+SGL_FrontController::init();
+
+//  main
+$output = new SGL_Output();
+$blockLoader = & new SGL_BlockLoader(0);
+$aBlocks = $blockLoader->render($output);
+foreach ($aBlocks as $key => $value) {
+    $blocksName = 'blocks'.$key;
+    $output->$blocksName = $value;
+}
+
+$tmpl = & new SGL_HtmlSimpleView($output);
+$html = $tmpl->render();
+print $html;
+/*
+require 'SGL/Output.php';
+require 'PEAR/Config.php';
+require 'Zend/Config.php';
+$api = $SGL_Api();
+$aBlocks = $api->getBlocks(12);
+$ok = $api->doLogin($user, $pass);
+*/
+
+require 'boostrap.php';
+?>
