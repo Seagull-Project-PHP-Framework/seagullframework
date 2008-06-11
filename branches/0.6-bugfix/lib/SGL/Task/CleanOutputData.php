80279cb1cb258f8db64cfebba674db43484265d8
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-06-11 11:07:08 +0000

    better handling for ajax msgs -1

---
+++ branches/0.6-bugfix/lib/SGL/Task/CleanOutputData.php
@@ -0,0 +1,36 @@
+<?php
+
+class SGL_Task_CleanOutputData extends SGL_DecorateProcess
+{
+    function process($input, $output)
+    {
+        $this->processRequest->process($input, $output);
+
+        //  catch session timeout
+        if (SGL_Error::count()) {
+            if (SGL_Error::getLast()->getCode() == SGL_ERROR_INVALIDSESSION) {
+                $aMsg = array(
+                  'message' => 'session timeout',
+                  'type'    => SGL_MESSAGE_ERROR,
+                );
+                $output->aMsg($aMsg);
+            }
+        }
+
+        SGL::logMessage(null, PEAR_LOG_DEBUG);
+
+        $data = new stdClass();
+        $aExceptions = array('aCssFiles', 'aHeaders',
+            'aJavascriptFiles', 'aOnLoadEvents', 'aOnUnloadEvents', 'aOnReadyDomEvents',
+            'onLoad', 'onReadyDom', 'onUnload', 'conf');
+        foreach ($output as $k => $v) {
+            if (!in_array($k, $aExceptions)) {
+                $data->$k = $v;
+            }
+            unset($output->$k);
+        }
+        $output->data = $data;
+    }
+}
+
+?>
