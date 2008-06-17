d21a128f3e676d3ef6afdc8bad5656651f370566
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-06-17 21:42:29 +0000

    installer now checks www dir is writable, fixes #1602

--- branches/0.6-bugfix/lib/SGL/Task/DetectEnv.php
+++ branches/0.6-bugfix/lib/SGL/Task/DetectEnv.php
@@ -321,11 +321,13 @@ class SGL_Task_GetFilesystemInfo extends SGL_EnvSummaryTask
         'installRoot' => array(SGL_NEUTRAL => 0),
         'varDirExists' => array(SGL_REQUIRED => 1),
         'varDirIsWritable' => array(SGL_REQUIRED => 1),
+        'wwwDirIsWritable' => array(SGL_REQUIRED => 1),
     );
     var $aErrors = array(
         'installRoot' => '',
         'varDirExists' => 'It appears you do not have a "var" folder, please create a folder with this name in the root of your Seagull install',
         'varDirIsWritable' => "Your \"var\" dir is not writable by the webserver, to make it writable type the following at the command line: chmod 777 %e",
+        'wwwDirIsWritable' => "Your \"www\" dir is not writable by the webserver, to make it writable type the following at the command line: chmod 777 %e",
     );
 
     function run()
@@ -333,6 +335,7 @@ class SGL_Task_GetFilesystemInfo extends SGL_EnvSummaryTask
         $this->aData['installRoot'] = SGL_PATH;
         $this->aData['varDirExists'] = bool2int(file_exists(SGL_VAR_DIR));
         $this->aData['varDirIsWritable'] = bool2int(is_writable(SGL_VAR_DIR));
+        $this->aData['wwwDirIsWritable'] = bool2int(is_writable(SGL_WEB_ROOT));
         return $this->render($this->aData);
     }
 }
