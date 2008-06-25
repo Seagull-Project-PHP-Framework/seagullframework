4b9050942c676bf5498be141014640512f8adcb9
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-06-25 14:09:21 +0000

    fix for windows -1

--- trunk/www/index.php
+++ trunk/www/index.php
@@ -39,7 +39,8 @@ if (!is_file($varDir . '/INSTALL_COMPLETE.php')) {
     $protocol = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on'
         ? 'https'
         : 'http';
-    $webRoot = $protocol . '://'. $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . '/setup.php';
+    $webRoot = $protocol . '://'. $_SERVER['HTTP_HOST'] .
+        str_replace('\\','/',(dirname($_SERVER['SCRIPT_NAME']))) . '/setup.php';
     header('Location: '.$webRoot);
     exit;
 } else {
