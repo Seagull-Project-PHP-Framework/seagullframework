eb34e7d32e07bf28b5e7e9a406a27351e1d95d22
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2010-04-03 17:00:15 +0000

    better error reporting for php 5.3

--- branches/0.6-bugfix/lib/SGL/Task/Init.php
+++ branches/0.6-bugfix/lib/SGL/Task/Init.php
@@ -701,7 +701,7 @@ class SGL_Task_EnsureFC extends SGL_Task
         //  load BC functions depending on PHP version detected
         if (version_compare(phpversion(), "5.3.0", '>=')) {
             date_default_timezone_set('UTC');
-            error_reporting(E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR);
+            error_reporting(E_COMPILE_ERROR|E_RECOVERABLE_ERROR|E_ERROR|E_CORE_ERROR|E_NOTICE|E_WARNING);
         }
     }
 }
