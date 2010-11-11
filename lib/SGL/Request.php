27857c0fd60d3d3771f83351b78c3791810a08a2
Demian Turner <demian@seagullsystems.com>
2010-11-11 14:44:49 +0000

    improving db error detection

--- lib/SGL/Request.php
+++ lib/SGL/Request.php
@@ -75,11 +75,8 @@ class SGL_Request
             }
             $obj = new $class();
             error_log('##########   Req type: '.$class);
-            $ok = $obj->init();
 
-            return PEAR::isError($ok)
-                ? $ok
-                : $obj;
+            return $obj;
         }
     }
 
