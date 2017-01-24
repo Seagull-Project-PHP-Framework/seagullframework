4211af58b3ec7fd199fed649a2aa76d4d1413c3f
demianturner <demianturner@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2017-01-24 07:17:20 +0000

    copying modules back into branch

---
+++ branches/0.6-bugfix/modules/contactus/lang/german-utf-8.php
@@ -0,0 +1,45 @@
+<?php
+    $words = array(
+/*  CONTACT US MGR  */
+        'Contact Us' => 'Treten Sie mit uns in Kontakt',
+        'First Name' => 'Vorname',
+        'Last Name' => 'Nachname',
+        'Email' => 'Email',
+        'contact intro' => 'Füllen Sie bitte die folgenden Felder aus und drücken Sie auf "Absenden"',
+        'Type of enquiry' => 'Art der Anfrage',
+        'Comment' => 'Kommentar',
+        'Contact Enquiry from' => 'Kontaktanfrage von',
+        'aContactType' => array(
+            'General enquiry' => 'Allgemeine Anfrage',
+            'Get a quote' => 'Get a quote',
+            'Hosting info' => 'Hosting info',
+            'Site feedback' => 'Feedback',
+            ),
+
+/*  ERROR MSGS  */
+        'You must enter your first name' => 'Bitte geben Sie Ihren Vornamen an',
+        'You must enter your last name' => 'Bitte geben Sie Ihren Nachnamen an',
+        'You must fill in your comment' => 'Bitte geben Sie Ihren Kommentar ein ',
+        'Your email is not correctly formatted' => 'Ihre Email-Adresse fehlerhaft',
+        'You must enter your email' => 'Bitte geben Sie Ihre Email-Adresse an.',
+    );
+?>
+<?php
+$words['Contact Us'] = 'Treten Sie mit uns in Kontakt';
+$words['First Name'] = 'Vorname';
+$words['Last Name'] = 'Nachname';
+$words['Email'] = 'Email';
+$words['contact intro'] = 'Füllen Sie bitte die folgenden Felder aus und drücken Sie auf \"Absenden\"';
+$words['Type of enquiry'] = 'Art der Anfrage';
+$words['Comment'] = 'Kommentar';
+$words['Contact Enquiry from'] = 'Kontaktanfrage von';
+$words['aContactType']['General enquiry'] = 'Allgemeine Anfrage';
+$words['aContactType']['Get a quote'] = 'Get a quote';
+$words['aContactType']['Hosting info'] = 'Hosting info';
+$words['aContactType']['Site feedback'] = 'Feedback';
+$words['You must enter your first name'] = 'Bitte geben Sie Ihren Vornamen an';
+$words['You must enter your last name'] = 'Bitte geben Sie Ihren Nachnamen an';
+$words['You must fill in your comment'] = 'Bitte geben Sie Ihren Kommentar ein ';
+$words['Your email is not correctly formatted'] = 'Ihre Email-Adresse fehlerhaft';
+$words['You must enter your email'] = 'Bitte geben Sie Ihre Email-Adresse an.';
+?>
